491f38b297f9d281881893a240b4c829b0b9d492
Michael Johnson <michael@icefoganalytics.com>
2021-05-17 16:10:47 -0700

    Update EducationHistory.vue

--- src/web/src/views/EducationHistory.vue
+++ src/web/src/views/EducationHistory.vue
@@ -1,30 +1,145 @@
 <template>
   <div class="home">
     <h1>Education History</h1>
-    <hr />
 
     <v-card class="default">
-      <v-card-title>Yukon Excellence Awards</v-card-title>
+      <v-card-title>Secondary Education History</v-card-title>
       <v-card-text>
         <div class="row">
-          <div class="col-md-12 py-0">
-            <v-switch
+          <div class="col-md-12">
+            <v-select
               outlined
               dense
+              background-color="white"
               hide-details
-              label="Applying for YEA reimbursement or direct payment"
-              v-model="is_applying"
-            ></v-switch>
+              label="High school"
+              v-model="high_school"
+              :items="highSchoolOptions"
+            ></v-select>
+          </div>
+          <div class="col-md-4">
+            <v-text-field
+              outlined
+              dense
+              background-color="white"
+              hide-details
+              label="High school city"
+              v-model="high_school_city"
+            ></v-text-field>
+          </div>
+          <div class="col-md-4">
+            <v-select
+              outlined
+              dense
+              background-color="white"
+              hide-details
+              label="High school province"
+              v-model="high_school_province"
+              :items="provinceOptions"
+            ></v-select>
+          </div>
+          <div class="col-md-4">
+            <v-select
+              outlined
+              dense
+              background-color="white"
+              hide-details
+              label="High school country"
+              v-model="high_school_country"
+              :items="countryOptions"
+            ></v-select>
           </div>
 
-          <div class="col-md-3">
+          <div class="col-md-2">
             <v-select
               outlined
               dense
               background-color="white"
               hide-details
               label="Left high school year"
-              v-model="high_school_year"
+              v-model="last_high_year"
+              :items="yearOptions"
+            ></v-select>
+          </div>
+          <div class="col-md-2">
+            <v-select
+              outlined
+              dense
+              background-color="white"
+              hide-details
+              label="Left high school month"
+              v-model="last_high_month"
+              :items="monthOptions"
+            ></v-select>
+          </div>
+          <div class="col-md-4">
+            <v-select
+              outlined
+              dense
+              background-color="white"
+              hide-details
+              label="Last grade completed"
+              v-model="last_grade_completed"
+              :items="gradeOptions"
+            ></v-select>
+          </div>
+          <div class="col-md-4">
+            <v-select
+              outlined
+              dense
+              background-color="white"
+              hide-details
+              label="Highest education level"
+              v-model="last_grade_completed"
+              :items="educationLevelOptions"
+            ></v-select>
+          </div>          
+        </div>
+      </v-card-text>
+    </v-card>
+
+    <hr class="mt-5 mb-5" />
+    <h3>Post-Secondary Education</h3>
+    <v-card class="default mb-5" v-for="(item, i) of post_secondary" :key="i">
+      <v-card-title
+        >Education {{ 1 + i }}
+        <v-spacer></v-spacer>
+        <v-btn color="warning" x-small fab class="my-0" @click="removeEducation(i)"
+          ><v-icon>mdi-close</v-icon></v-btn
+        >
+      </v-card-title>
+      <v-card-text>
+        <div class="row">
+          <div class="col-md-6">
+            <v-select
+              outlined
+              dense
+              background-color="white"
+              hide-details
+              label="Institution"
+              v-model="item.institution"
+              :items="institutionOptions"
+            ></v-select>
+          </div>
+          <div class="col-md-6">
+            <v-select
+              outlined
+              dense
+              background-color="white"
+              hide-details
+              label="Study area"
+              v-model="item.study_area"
+              :items="studyAreaOptions"
+            ></v-select>
+          </div>
+          <div class="col-md-3">
+            <v-select
+              outlined
+              dense
+              background-color="white"
+              hide-details
+              label="From year"
+              v-model="item.from_year"
               :items="yearOptions"
             ></v-select>
           </div>
@@ -34,156 +149,37 @@
               dense
               background-color="white"
               hide-details
-              label="Left high school month"
-              v-model="high_school_month"
+              label="From month"
+              v-model="item.from_month"
               :items="monthOptions"
             ></v-select>
           </div>
           <div class="col-md-3">
-            <v-text-field
+            <v-select
               outlined
               dense
               background-color="white"
               hide-details
-              label="YEA request amount"
-              v-model="yea_request_amount"
-              v-currency="{ currency: 'USD', locale: 'en' }"
-            ></v-text-field>
+              label="To year"
+              v-model="item.to_year"
+              :items="yearOptions"
+            ></v-select>
           </div>
           <div class="col-md-3">
-            <v-text-field
-              outlined
-              dense
-              disabled
-              background-color="white"
-              hide-details
-              label="Current YEA balance"
-              v-model="yea_balance"
-              v-currency="{ currency: 'USD', locale: 'en' }"
-            ></v-text-field>
-          </div>
-
-          <div class="col-md-3">
-            <v-menu
-              v-model="yea_expiry_menu"
-              :close-on-content-click="false"
-              transition="scale-transition"
-              left
-              nudge-top="26"
-              offset-y
-              min-width="auto"
-            >
-              <template v-slot:activator="{ on, attrs }">
-                <v-text-field
-                  v-model="yea_expiry_date"
-                  label="YEA expiry date"
-                  append-icon="mdi-calendar"
-                  readonly
-                  outlined
-                  hide-details
-                  dense
-                  background-color="white"
-                  v-bind="attrs"
-                  v-on="on"
-                ></v-text-field>
-              </template>
-              <v-date-picker
-                v-model="yea_expiry_date"
-                @input="yea_expiry_menu = false"
-              ></v-date-picker>
-            </v-menu>
-          </div>
-          <div class="col-md-3">
-            <v-text-field
+            <v-select
               outlined
               dense
               background-color="white"
               hide-details
-              label="Yukon ID"
-              v-model="yukon_id"
-            ></v-text-field>
-          </div>
-
-          <div class="col-md-12">
-            <v-textarea
-              outlined
-              dense
-              background-color="white"
-              hide-details
-              label="Comment"
-              v-model="comments"
-            ></v-textarea>
+              label="To month"
+              v-model="item.to_month"
+              :items="monthOptions"
+            ></v-select>
           </div>
         </div>
       </v-card-text>
     </v-card>
-
-    <hr class="mt-5 mb-5" />
-    <v-card class="default mb-5" v-for="(item, i) of earned" :key="i">
-      <v-card-title
-        >Earned {{ 1 + i }}
-        <v-spacer></v-spacer>
-        <v-btn color="warning" x-small fab class="my-0" @click="removeEarned(i)"
-          ><v-icon>mdi-close</v-icon></v-btn
-        >
-      </v-card-title>
-      <v-card-text>
-        <div class="row">
-          <div class="col-md-3">
-            <v-text-field
-              outlined
-              dense
-              background-color="white"
-              hide-details
-              label="First name"
-              v-model="item.first_name"
-            ></v-text-field>
-          </div>
-          <div class="col-md-3">
-            <v-text-field
-              outlined
-              dense
-              background-color="white"
-              hide-details
-              label="Last name"
-              v-model="item.last_name"
-            ></v-text-field>
-          </div>
-          <div class="col-md-6">
-            <v-text-field
-              outlined
-              dense
-              background-color="white"
-              hide-details
-              label="Course"
-              v-model="item.course"
-            ></v-text-field>
-          </div>
-          <div class="col-md-3">
-            <v-text-field
-              outlined
-              dense
-              background-color="white"
-              hide-details
-              label="Amount"
-              v-model="item.amount"
-              v-currency="{ currency: 'USD', locale: 'en' }"
-            ></v-text-field>
-          </div>
-          <div class="col-md-3">
-            <v-text-field
-              outlined
-              dense
-              background-color="white"
-              hide-details
-              label="Year"
-              v-model="item.year"
-            ></v-text-field>
-          </div>
-        </div>
-      </v-card-text>
-    </v-card>
-    <v-btn color="info" @click="addEarned()">Add YEA earned</v-btn>
+    <v-btn color="info" @click="addEducation()">Add post-secondary education</v-btn>
 
     <confirm-dialog ref="confirm"></confirm-dialog>
   </div>
@@ -193,20 +189,26 @@
 import moment from "moment";
 export default {
   data: () => ({
+    countryOptions: ["Canada", "United States"],
+    provinceOptions: ["Yukon", "British Columbia"],
     yearOptions: [],
     monthOptions: [],
-    yea_expiry_menu: null,
+    gradeOptions: ["13", "12", "11", "10", "9", "8", "7", "6", "5"],
+    highSchoolOptions: ["FH Collins"],
+    educationLevelOptions: ["High school", "Some University Undergraduate"],
+    institutionOptions: ["Yukon U"],
+    studyAreaOptions: ["Business administration"],
 
-    is_applying: false,
-    high_school_year: "",
-    high_school_month: "06",
-    yea_request_amount: 0,
-    yea_balance: 0,
-    yea_expiry_date: null,
-    yukon_id: "",
-    comments: "",
+    high_school: "",
+    high_school_city: "",
+    high_school_province: "",
+    high_school_country: "",
+    last_high_year: "",
+    last_high_month: "",
+    last_grade_completed: "",
+    eduction_level: "",
 
-    earned: [],
+    post_secondary: [],
   }),
   async created() {
     this.monthOptions = [];
@@ -225,15 +227,15 @@ export default {
     }
   },
   methods: {
-    addEarned() {
-      this.earned.push({amount: 0});
+    addEducation() {
+      this.post_secondary.push({ amount: 0 });
     },
-    removeEarned(index) {
+    removeEducation(index) {
       this.$refs.confirm.show(
         "Are you sure?",
-        "Click 'Confirm' below to permanently remove this YEA.",
+        "Click 'Confirm' below to permanently remove this post-secondary education.",
         () => {
-          this.earned.splice(index, 1);
+          this.post_secondary.splice(index, 1);
         },
         () => {}
       );
