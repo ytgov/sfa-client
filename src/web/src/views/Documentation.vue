529728dd394572e3f25a72bb6a366cb77b58bf92
Michael Johnson <michael@icefoganalytics.com>
2021-05-18 17:28:47 -0700

    Update Documentation.vue

--- src/web/src/views/Documentation.vue
+++ src/web/src/views/Documentation.vue
@@ -1,40 +1,113 @@
 <template>
   <div class="home">
-    <h1>Documentation</h1>
-    <hr />
+    <h1>Documentation Required and Received</h1>
 
-    <v-tabs v-model="tab" background-color="#fff2d5" color="primary">
-      <v-tab key="0">Contact</v-tab>
-      <v-tab key="1">Basic</v-tab>
-      <v-tab key="1">Statistical</v-tab>
-      <v-tab key="1">Ids / Pre-system info</v-tab>
-      <v-tab key="1">Next of kin / consent</v-tab>
-    </v-tabs>
+    <v-card class="default mb-5">
+      <v-card-text>
+        <div class="row">
+          <div class="col-md-12">
+            <h3>Documentation</h3>
+            <div v-for="(item, i) of documents" :key="i" class="row">
+              <div class="col-md-6">
+                <v-select
+                  outlined
+                  dense
+                  background-color="white"
+                  hide-details
+                  label="Documentation"
+                  v-model="item.documentation"
+                  :items="documentationOptions"
+                ></v-select>
+              </div>
+              <div class="col-md-3">
+                <v-menu
+                  v-model="item.received_date_menu"
+                  :close-on-content-click="false"
+                  transition="scale-transition"
+                  left
+                  nudge-top="26"
+                  offset-y
+                  min-width="auto"
+                >
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-text-field
+                      v-model="item.received_date"
+                      label="Received date"
+                      append-icon="mdi-calendar"
+                      hide-details
+                      readonly
+                      outlined
+                      dense
+                      background-color="white"
+                      v-bind="attrs"
+                      v-on="on"
+                    ></v-text-field>
+                  </template>
+                  <v-date-picker
+                    v-model="item.received_date"
+                    @input="item.received_date_menu = false"
+                  ></v-date-picker>
+                </v-menu>
+              </div>
 
-    <v-tabs-items v-model="tab" style="padding: 20px">
-      <v-tab-item key="0">
-      </v-tab-item>
-      <v-tab-item key="1">
-      </v-tab-item>
-    </v-tabs-items>
+              <div class="col-md-2 pt-0">
+                <v-switch
+                  outlined
+                  dense
+                  hide-details
+                  label="Show online"
+                  v-model="item.show_online"
+                ></v-switch>
+              </div>
+
+              <div class="col-md-1">
+                <v-btn
+                  color="warning"
+                  x-small
+                  fab
+                  title="Remove"
+                  class="my-0 float-right"
+                  @click="removeDocumentation(i)"
+                  ><v-icon>mdi-close</v-icon></v-btn
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+      </v-card-text>
+    </v-card>
+
+    <v-btn color="info" @click="addDocumentation()">Add documentation</v-btn>
+
+    <confirm-dialog ref="confirm"></confirm-dialog>
   </div>
 </template>
 
 <script>
-import router from "../router";
-import store from "../store";
-
 export default {
   name: "Home",
   data: () => ({
-    tab: 0,
+    documentationOptions: [
+      "YG Application",
+      "Official Transcript - Original document (must be mailed)",
+    ],
+    documents: [],
   }),
-  async created() {
-    await store.dispatch("checkAuthentication");
-    var isAuth = store.getters.isAuthenticated;
-
-    if (isAuth) router.push("/dashboard");
+  async created() {},
+  methods: {
+    addDocumentation() {
+      this.documents.push({ birth_date: "" });
+    },
+    removeDocumentation(index) {
+      this.$refs.confirm.show(
+        "Are you sure?",
+        "Click 'Confirm' below to permanently remove this documentation.",
+        () => {
+          this.documents.splice(index, 1);
+        },
+        () => {}
+      );
+    },
   },
-  methods: {},
 };
 </script>
