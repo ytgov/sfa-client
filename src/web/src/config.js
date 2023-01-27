37f21cfa79a963dc3a6ec59c64694bb1c27fb5d5
Mariazel Hernandez <mariazel@bizont.ca>
2023-01-27 12:14:43 -0700

    Change IP to localhost

--- src/web/src/config.js
+++ src/web/src/config.js
@@ -54,4 +54,4 @@ export const sections = [
     },
 ];
 export const environment = process.env.NODE_ENV;
-export const apiBaseUrl = (process.env.NODE_ENV == "test" || process.env.NODE_ENV == "production") ? "" : "http://35.93.91.213:3000";
+export const apiBaseUrl = (process.env.NODE_ENV == "test" || process.env.NODE_ENV == "production") ? "" : "http://localhost:3000";
