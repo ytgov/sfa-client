dbbcea73e5e5b822d8e78414abff747fc422d8bb
Emanuel Pastrana <emanuel@bizont.ca>
2023-03-17 10:54:01 -0600

    education and demographics forms

--- src/web/src/components/application/EducationForm.vue
+++ src/web/src/components/application/EducationForm.vue
@@ -13,6 +13,8 @@
                             background-color="white" 
                             hide-details 
                             label="Pre-Study Classiﬁcation"
+                            v-model="application.csl_classification"
+                            :items="cslClasificationOptions"
                         >
                         </v-select>
                     </div>
@@ -23,6 +25,8 @@
                             background-color="white" 
                             hide-details 
                             label="Study Classiﬁcation"
+                            v-model="application.prestudy_csl_classification"
+                            :items="cslClasificationOptions"
                         >
                         </v-select>
                     </div>
@@ -36,14 +40,18 @@
                 </h1>
                 <div class="row">
                     <div class="col-md-10">
-                        <v-select 
+                        <v-autocomplete 
                             outlined 
                             dense 
                             background-color="white" 
                             hide-details 
                             label="High School Attended"
+                            :items="highSchools"
+                            item-text="name"
+                            item-value="id"
+                            v-model="student.high_school_id"
                         >
-                        </v-select>
+                        </v-autocomplete>
                     </div>
                     <div class="col-md-2 d-flex justify-end">
                         <v-btn
@@ -67,6 +75,8 @@
                             background-color="white" 
                             hide-details 
                             label="Year"
+                            v-model="student.high_school_left_year"
+                            :items="yearOptions"
                         >
                         </v-select>
                     </div>
@@ -77,6 +87,8 @@
                             background-color="white" 
                             hide-details 
                             label="Month"
+                            v-model="student.high_school_left_month"
+                            :items="monthOptions"
                         >
                         </v-select>
                     </div>
@@ -93,14 +105,31 @@ import validator from "@/validator";
 
 export default {
   computed: {
+    ...mapGetters(["highSchools", "monthOptions", "yearOptions"]),
+    application: function () {
+      return store.getters.selectedApplication;
+    },
+    student: function () {
+      return store.getters.selectedStudent;
+    },
   },
   data() {
     return {
-
+        cslClasificationOptions: [
+            {text: "Single Dependent", value: 1},
+            {text: "Single Independent - 2 year workforce", value: 2},
+            {text: "Single Independent - 4 year high school", value: 3},
+            {text: "Married / Common Law", value: 4},
+            {text: "Single Parent", value: 5},
+        ],
     };
+
+    
   },
   async created() {
-
+    store.dispatch("setHighSchools");
+    store.dispatch("setMonthOptions");
+    store.dispatch("setYearsOptions");
   },
   watch: {
 
--- src/web/src/components/application/StatisticalForm.vue
+++ src/web/src/components/application/StatisticalForm.vue
@@ -11,6 +11,11 @@
               background-color="white"
               hide-details
               label="Language"
+              v-model="student.language_id"
+              @change="doSaveStudent('language_id', student.language_id, 'personInfo', student.id)"
+              :items="languages"
+              item-value="id"
+              item-text="description"
             ></v-select>
           </div>
           <div class="col-md-3">
@@ -20,10 +25,16 @@
               background-color="white"
               hide-details
               label="Gender"
+              v-model="student.sex_id"
+              @change="doSaveStudent('sex_id', student.sex_id, 'personInfo', student.id)"
+              :items="sexes"
+              item-value="id"
+              item-text="description"
             ></v-select>
           </div>
           <div class="col-md-3">
             <v-menu
+                v-model="show_menu"
                 :close-on-content-click="false"
                 transition="scale-transition"
                 left
@@ -33,8 +44,9 @@
               >
                 <template v-slot:activator="{ on, attrs }">
                   <v-text-field
-                    label="Date app received"
+                    label="Birth Date"
                     append-icon="mdi-calendar"
+                    :value="student.birth_date?.slice(0, 10)"
                     hide-details
                     readonly
                     outlined
@@ -45,6 +57,12 @@
                   ></v-text-field>
                 </template>
                 <v-date-picker
+                :value="student.birth_date?.slice(0, 10)"
+                @input="e => {
+                  student.birth_date = e;
+                  show_menu = false;
+                }"
+                @change="doSaveStudent('birth_date', student.birth_date, 'personInfo', student.id)"
                 ></v-date-picker>
               </v-menu>
           </div>
@@ -79,10 +97,10 @@
               background-color="white"
               hide-details
               label="Citizenship"
-              v-model="application.CITIZENSHIP_STATUS"
+              v-model="application.citizenship_status"
               :items="citizenshipOptions"
               @change="
-                doSaveApp('CITIZENSHIP_STATUS', application.CITIZENSHIP_STATUS)
+                doSaveApp('citizenship_status', application.citizenship_status)
               "
             ></v-select>
           </div>
@@ -163,6 +181,8 @@
               background-color="white"
               hide-details
               label="Notes"
+              v-model="application.stat_info_comment"
+              @change="doSaveApp('stat_info_comment', application.stat_info_comment)"
             >
 
             </v-textarea>
@@ -172,6 +192,8 @@
     </v-card>
 
     <v-switch
+      v-model="application.has_consent_to_share_data"
+      @change="doSaveApp('has_consent_to_share_data', application.has_consent_to_share_data)"
       label="STEP and Grad CORP data sharing consent"
     >
 
@@ -185,7 +207,7 @@ import { mapGetters } from 'vuex';
 
 export default {
   computed: {
-    ...mapGetters(["maritalStatusList", "aboriginalStatusList", "firstNations"]),
+    ...mapGetters(["maritalStatusList", "aboriginalStatusList", "firstNations", "languages", "sexes"]),
     application: function () {
       return store.getters.selectedApplication;
     },
@@ -194,11 +216,12 @@ export default {
     },
   },
   data: () => ({
+    show_menu: false,
     maritalOptions: [],
     aboriginalStatusOptions: [],
     firstNationOptions: [],
     citizenshipOptions: [
-      { vale: null, text: "Not Recorded" },
+      { vale: 0, text: "Not Recorded" },
       { value: 1, text: "Canadian" },
       { value: 2, text: "Permanent Resident" },
       { value: 3, text: "Protected Person" },
@@ -208,6 +231,9 @@ export default {
   async created() {
     store.dispatch("setMaritalStatusList");
     store.dispatch("setAboriginalStatusList");
+    store.dispatch("setFirstNations");
+    store.dispatch("setLanguages");
+    store.dispatch("setSexes");
     store.dispatch("setFirstNations");
     //this.updateView(this.application);
   },
