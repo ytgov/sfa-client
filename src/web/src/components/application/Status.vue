e2804ec3c23d07c6710fd6c3421e073b018ffee9
Emanuel Pastrana <emanuel@bizont.ca>
2023-05-05 18:10:19 -0600

    funding type disabled and added a label with the name of the funding type

--- src/web/src/components/application/Status.vue
+++ src/web/src/components/application/Status.vue
@@ -5,15 +5,14 @@
       <v-card class="default mb-5" v-for="item, index in application.funding_requests" :key="index">
         <v-card-text>
           <div class="row">
-            <div class="col-md-4">
+            <div class="col-md-4" :title="fundingTypeOptions?.find(ft => ft.REQUEST_TYPE_ID === item?.request_type_id)?.DESCRIPTION || 'Funding Status' ">
               <v-select
-                :disabled="showAdd"
+                disabled
                 outlined
                 dense
                 background-color="white"
                 hide-details
                 label="Funding Type"
-                @change="updateFundingRequest({ request_type_id: item.request_type_id }, item.id)"
                 v-model="item.request_type_id"
                 :items="fundingTypeOptions"
                 item-text="DESCRIPTION"
