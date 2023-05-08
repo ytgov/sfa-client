00cb54abfd303c4d2d9e46d1172b3f6300aa913b
Emanuel Pastrana <emanuel@bizont.ca>
2023-05-08 15:13:41 -0600

    new functions added

--- db/4_functions.sql
+++ db/4_functions.sql
@@ -1,4 +1,5 @@
 -- STA Upgrading Weeks - Pre-Legislation Chg
+--before get_prev_pre_leg_weeks
 CREATE OR ALTER FUNCTION sfa.fn_get_pre_leg_sta_up_weeks 
 (
     @student_id INT
@@ -77,7 +78,7 @@ BEGIN
 END
 
 GO
-
+-- This function is used to get the previous pre-legislation YG/STA weeks prior to current HD non upgrading                
 CREATE OR ALTER FUNCTION sfa.fn_get_prev_pre_leg_weeks(@student_id_p INT, @application_id_p INT)
 RETURNS NUMERIC
 AS 
@@ -108,10 +109,11 @@ END
 
 GO
 
-CREATE OR ALTER FUNCTION sfa.fn_get_funded_years_used_preleg_chg(
-	@student_id_p INT, 
-	@application_id_p INT
-)
+/* This function is used to get the current system total years funded
+    for the particular student not counting the given history detail
+    before get_funded_years_count
+*/
+CREATE OR ALTER FUNCTION sfa.fn_get_funded_years_used_preleg_chg( @student_id_p INT,  @application_id_p INT)
 RETURNS NUMERIC
 AS 
 BEGIN
@@ -134,12 +136,186 @@ END
 
 GO
 
+-- This function is used to get the post-legislation YG/STA weeks prior to current HD non upgrading 
+CREATE OR ALTER FUNCTION sfa.fn_get_prev_post_leg_weeks(@student_id_p INT, @application_id_p INT)
+RETURNS NUMERIC
+AS 
+BEGIN
+    DECLARE  @v_num_weeks NUMERIC = 0;
 
+    SELECT @v_num_weeks =  COALESCE(sum(a.weeks_allowed),0)
+    FROM sfa.application app
+    INNER JOIN sfa.funding_request fr ON app.id = fr.application_id
+    INNER JOIN (SELECT funding_request_id
+                    , assessment_id
+                    , sum(disbursed_amount) disbursed_amount
+                    FROM sfa.disbursement
+            GROUP BY funding_request_id, assessment_id) d ON fr.id = d.funding_request_id
+    INNER JOIN sfa.assessment a ON  d.assessment_id = a.id
+    WHERE  app.program_id <> (SELECT id FROM sfa.program WHERE description = 'Upgrading-Academic')
+    AND app.student_id = @student_id_p
+    AND app.id <  @application_id_p
+    AND app.academic_year_id  > 2015
+    AND d.disbursed_amount > 0 -- positive disbursement
+    group by app.student_id;
 
-   
-               
-               
-               
-               
+    RETURN @v_num_weeks;        
+END
+GO
 
+/* This function is used to get the pre-legislation Student Training Allowance 
+   upgrading weeks weeks prior to current HD */
+CREATE OR ALTER FUNCTION sfa.fn_get_prev_pre_leg_sta_up_weeks(@student_id_p INT,  @application_id_p INT)
+RETURNS NUMERIC
+AS 
+BEGIN               
+       DECLARE @v_num_weeks NUMERIC = 0;
 
+            SELECT @v_num_weeks =  COALESCE(sum(a.weeks_allowed), 0 )
+            FROM sfa.application app
+            INNER JOIN sfa.funding_request fr ON app.id = fr.application_id
+            INNER JOIN (SELECT funding_request_id
+                            , assessment_id
+                            , sum(disbursed_amount) disbursed_amount
+                        FROM sfa.disbursement
+                    GROUP BY funding_request_id, assessment_id) d ON fr.id = d.funding_request_id
+            INNER JOIN sfa.assessment a ON  d.assessment_id = a.id
+            WHERE  app.student_id = @student_id_p
+            AND app.id < @application_id_p
+            AND app.academic_year_id <=2015
+            AND app.program_id = (SELECT id FROM sfa.program WHERE description = 'Upgrading-Academic')  -- upgrading program
+            AND d.disbursed_amount > 0 -- positive disbursement
+            AND fr.request_type_id = 1 -- request type STA
+            group by app.student_id;
+
+            RETURN CEILING(@v_num_weeks);              
+
+END
+GO
+
+/* This function is used to get the post-legislation Student Training Allowance upgrading weeks */
+CREATE OR ALTER FUNCTION sfa.fn_get_prev_post_leg_sta_up_weeks(@student_id_p INT,  @application_id_p INT)
+RETURNS NUMERIC
+AS 
+BEGIN                   
+    DECLARE @v_num_weeks NUMERIC = 0;
+
+                SELECT @v_num_weeks =  COALESCE(sum(a.weeks_allowed), 0 )
+                FROM sfa.application app
+                INNER JOIN sfa.funding_request fr ON app.id = fr.application_id
+                INNER JOIN (SELECT funding_request_id
+                              , assessment_id
+                              , sum(disbursed_amount) disbursed_amount
+                         FROM sfa.disbursement
+                      GROUP BY funding_request_id, assessment_id) d ON fr.id = d.funding_request_id
+                INNER JOIN sfa.assessment a ON d.assessment_id = a.id
+                WHERE app.student_id = @student_id_p
+                AND app.id < @application_id_p
+                AND app.academic_year_id > 2015
+                AND app.program_id = (SELECT id FROM sfa.program WHERE description = 'Upgrading-Academic')  -- upgrading program
+                AND d.disbursed_amount > 0 -- positive disbursement
+                AND fr.request_type_id = 1 -- request type STA
+                group by app.student_id;
+
+                RETURN @v_num_weeks;
+
+END
+GO 
+
+/* This function is used to get the post-legislation Student Training Allowance  
+   upgrading weeks prior to current HD */
+CREATE OR ALTER FUNCTION sfa.fn_get_post_leg_sta_up_weeks(@student_id_p INT)
+RETURNS NUMERIC
+AS 
+BEGIN                   
+    DECLARE @v_num_weeks NUMERIC = 0;
+
+        SELECT @v_num_weeks = COALESCE(sum(a.weeks_allowed),0)
+        FROM sfa.application app
+        INNER JOIN sfa.funding_request fr ON app.id = fr.application_id
+        INNER JOIN (SELECT funding_request_id
+                        , assessment_id
+                        , sum(disbursed_amount) disbursed_amount
+                    FROM sfa.disbursement
+                GROUP BY funding_request_id, assessment_id) d ON fr.id = d.funding_request_id
+        INNER JOIN sfa.assessment a ON d.assessment_id = a.id
+        WHERE app.student_id = @student_id_p
+        AND app.academic_year_id > 2015
+        AND app.program_id = (SELECT id FROM sfa.program WHERE description = 'Upgrading-Academic')  -- upgrading program
+        AND d.disbursed_amount > 0 -- positive disbursement
+        AND fr.request_type_id = 1 -- request type STA
+        group by app.student_id;
+
+        RETURN @v_num_weeks;
+
+END
+GO
+
+/* This function is used to get the post-legislation Yukon Grant outside travel count */      
+CREATE OR ALTER FUNCTION sfa.fn_get_post_leg_outside_travel(@student_id_p INT)
+RETURNS NUMERIC
+AS 
+BEGIN                   
+    DECLARE @v_count NUMERIC = 0;
+        SELECT @v_count = count(a.airfare_amount)
+        FROM sfa.application app
+        INNER JOIN sfa.funding_request fr ON  app.id = fr.application_id
+        INNER JOIN (SELECT funding_request_id
+                        , assessment_id
+                        , sum(disbursed_amount) disbursed_amount
+                    FROM sfa.disbursement
+                GROUP BY funding_request_id, assessment_id) d ON fr.id = d.funding_request_id
+        INNER JOIN sfa.assessment a ON d.assessment_id = a.id
+            WHERE app.student_id = @student_id_p
+            AND app.academic_year_id > 2015
+            AND d.disbursed_amount > 0 -- positive disbursement
+            AND a.airfare_amount > 0 -- has an airfair amount
+            AND fr.request_type_id = 2 -- request type YG
+        GROUP BY app.student_id;
+
+        RETURN @v_count;
+
+END
+GO
+
+--AASSESSMENT SFA
+CREATE OR ALTER FUNCTION sfa.fn_get_previous_weeks_yg(@program_p VARCHAR, @student_id_p INT,  @application_id_p INT) 
+RETURNS NUMERIC
+AS 
+BEGIN
+    DECLARE @v_pre_leg_weeks NUMERIC = 0;
+    DECLARE @v_post_leg_weeks NUMERIC = 0;
+    DECLARE @v_total_weeks NUMERIC = 0;
+    DECLARE @v_adjusted_weeks NUMERIC = 0;
+
+		IF @student_id_p IS NOT NULL AND @application_id_p IS NOT NULL
+			BEGIN
+                IF @program_p = 'Upgrade'
+                    BEGIN
+                        SELECT @v_post_leg_weeks =  COALESCE(sfa.fn_get_prev_post_leg_sta_up_weeks(@student_id_p,  @application_id_p),0);
+        
+                        SELECT @v_pre_leg_weeks =  COALESCE( sfa.fn_get_prev_pre_leg_sta_up_weeks(@student_id_p,  @application_id_p),0);
+                        
+                        SELECT  @v_adjusted_weeks = COALESCE(adj_sta_upgrading_weeks,0) 
+                        FROM sfa.student  
+                        WHERE id =  @student_id_p;
+                    END
+                ELSE
+                    BEGIN
+                        SELECT @v_post_leg_weeks = COALESCE(sfa.fn_get_prev_post_leg_weeks(@student_id_p,  @application_id_p),0);
+                        SELECT @v_pre_leg_weeks = COALESCE( sfa.fn_get_prev_pre_leg_weeks(@student_id_p,  @application_id_p),0);
+                        
+                        SELECT @v_adjusted_weeks = COALESCE(adj_yg_funding_weeks,0) 
+                        FROM sfa.student  
+                        WHERE id = @student_id_p;
+                    END
+			    
+	   	    END
+		
+
+		set @v_total_weeks = @v_pre_leg_weeks + @v_post_leg_weeks + @v_adjusted_weeks;
+
+		RETURN @v_total_weeks;
+
+END
+GO
