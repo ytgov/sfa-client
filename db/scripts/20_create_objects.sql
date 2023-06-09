8b6302a5f36a46955dc57cd09366f2f61e73013d
Mario Lizarraga <mariolizarraga07@gmail.com>
2023-06-09 00:20:16 -0600

    Update script

--- db/scripts/20_create_objects.sql
+++ db/scripts/20_create_objects.sql
@@ -114,6 +114,18 @@ CREATE TABLE sfa.institution_campus_notes
     create_user_id        INT          NOT NULL REFERENCES sfa.[user]
 )
 
+-- ACCOMMODATION TYPE
+CREATE TABLE sfa.accommodation_type (
+	id int NOT NULL IDENTITY(1,1),
+	description nvarchar(200) NOT NULL,
+	is_active bit NOT NULL DEFAULT (1)
+)
+
+SET IDENTITY_INSERT sfa.accommodation_type ON
+INSERT INTO [sfa].[accommodation_type] (id, description) VALUES (1, 'Living at Parents')
+INSERT INTO [sfa].[accommodation_type] (id, description) VALUES (2, 'Living on Own')
+INSERT INTO [sfa].[accommodation_type] (id, description) VALUES (3, 'Both')
+SET IDENTITY_INSERT sfa.accommodation_type OFF
 
 -- SEX
 CREATE TABLE sfa.sex
