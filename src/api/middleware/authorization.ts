32269cbcdd931334bf98cc414027f01f1a52b109
Michael Johnson <michael@icefoganalytics.com>
2023-06-27 19:03:37 -0700

    Build fix

--- src/api/middleware/authorization.ts
+++ src/api/middleware/authorization.ts
@@ -1,35 +1,28 @@
 import { Request, Response, NextFunction } from "express";
 import { User } from "../models";
 
-const USER_ACTIVE_STATUS = "Active";
-
 export const UserRoles = {
-    ADMINISTRATOR: "Administrator",
+  ADMINISTRATOR: "Admin",
 };
 
-export const UserRoleOptions = [
-    UserRoles.ADMINISTRATOR,
-]
+export const UserRoleOptions = [UserRoles.ADMINISTRATOR];
 
+// this function should likely not be used
 export function authorize(roles: string[] = [], allowPending: boolean = false) {
-    return (req: Request, res: Response, next: NextFunction) => {
-        let currentUser = req.user as User;
+  return (req: Request, res: Response, next: NextFunction) => {
+    let currentUser = req.user as User;
 
-        if (!req.oidc.isAuthenticated() || !currentUser)
-            return res.status(401).send('Not authenticated');
+    if (!req.oidc.isAuthenticated() || !currentUser) return res.status(401).send("Not authenticated");
 
-        if (currentUser.status != USER_ACTIVE_STATUS && !allowPending)
-            return res.status(401).json({ message: "Unauthorized - User inactive" });
+    if (currentUser.is_active) return res.status(401).json({ message: "Unauthorized - User inactive" });
 
-        // if route only requires an active user
-        if (roles.length == 0)
-            return next();
+    // if route only requires an active user
+    if (roles.length == 0) return next();
 
-        for (let role of roles) {
-            if (currentUser.roles && currentUser.roles.indexOf(role) >= 0)
-                return next();
-        }
+    for (let role of roles) {
+      if (currentUser.roles && currentUser.roles.indexOf(role) >= 0) return next();
+    }
 
-        return res.status(401).json({ message: 'Unauthorized - Missing role(s): ' + roles });
-    };
+    return res.status(401).json({ message: "Unauthorized - Missing role(s): " + roles });
+  };
 }
--- src/api/models/auth/index.ts
+++ src/api/models/auth/index.ts
@@ -7,6 +7,7 @@ export interface AuthUser {
   email: string;
   email_verified: boolean;
   sub: string;
+  is_active?: boolean;
 }
 
 export namespace AuthUser {
