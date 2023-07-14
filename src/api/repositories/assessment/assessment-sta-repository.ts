c908de53fac3abc84db3196967cbf5f16c4f38e5
Emanuel Pastrana <emanuel@bizont.ca>
2023-07-14 14:25:34 -0600

    assessmentSTA changes saved

--- src/api/repositories/assessment/assessment-sta-repository.ts
+++ src/api/repositories/assessment/assessment-sta-repository.ts
@@ -1,8 +1,14 @@
 import { Knex } from "knex";
 import moment from "moment";
-import { AssessmentDTO, ApplicationDTO, DisbursementDTO } from "models";
 import { AssessmentBaseRepository } from "./assessment-base-repository";
 import { ApplicationRepository } from "../application";
+import {
+    ApplicationDTO,
+    assessmentColumns,
+    AssessmentDTO,
+    DisbursementDTO,
+    AssessmentTable,
+} from "../../models";
 
 export class AssessmentSTA extends AssessmentBaseRepository {
 
@@ -14,16 +20,26 @@ export class AssessmentSTA extends AssessmentBaseRepository {
         this.applicationRepo = new ApplicationRepository(maindb);
     }
 
+    getAssessmentTable(assessment: AssessmentDTO): AssessmentTable {
+        return Object.keys(assessment)
+            .filter(key => assessmentColumns.includes(key as keyof AssessmentTable))
+            .reduce((obj: any, key) => {
+                obj[key as keyof AssessmentTable] = assessment[key as keyof AssessmentTable];
+                return obj as AssessmentTable;
+            }, {});
+    }
+
     async getNewInfo(
         application_id: number,
         funding_request_id: number,
-
+        disbursementList: DisbursementDTO[],
     ): Promise<AssessmentDTO> {
 
         let initValues: AssessmentDTO = {};
 
         this.application = await this.applicationRepo.getApplicationById(application_id);
 
+        initValues.funding_request_id = funding_request_id;
         initValues.assessed_date = moment().toDate();
         initValues.effective_rate_date = this.application.classes_start_date;
         initValues.classes_start_date = this.application.classes_start_date;
@@ -38,16 +54,26 @@ export class AssessmentSTA extends AssessmentBaseRepository {
             initValues.home_city_id || 0,
             initValues.destination_city_id || 0
         ]);
+        initValues.weekly_amount = await this.getScalarValue<number>("fn_get_weekly_amount_sta", [application_id]);
         initValues.assessed_amount = await this.getScalarValue<number>("fn_get_other_inst_total_sta", [
             initValues.weekly_amount || 0,
             initValues.second_residence_rate,
             0, // initValues.entitlement_days
             initValues.travel_allowance
         ]);
-        initValues.previous_disbursement = await this.getScalarValue<number>("fn_get_disbursed_amount_fct", [
-            funding_request_id,
-            0 // assessment_id because is a preview
-        ]);
+        
+        if (disbursementList.length) {
+            let disbursedAmounts = disbursementList.map((d) => {
+                return d.disbursed_amount ?? 0;
+            });
+            initValues.previous_disbursement = disbursedAmounts.reduce((a, b) => a + b, 0);
+        } else {
+            initValues.previous_disbursement = await this.getScalarValue<number>("fn_get_disbursed_amount_fct", [
+                funding_request_id,
+                0 // assessment_id because is a preview
+            ]);
+        }
+
         initValues.net_amount = await this.getScalarValue<number>("fn_get_net_sta", [
             initValues.assessed_amount || 0,
             initValues.previous_disbursement || 0,
@@ -75,10 +101,153 @@ export class AssessmentSTA extends AssessmentBaseRepository {
             this.application.student_id || 0,
             this.application.id || 0
         ]);
-        initValues.weekly_amount = await this.getScalarValue<number>("fn_get_weekly_amount_sta", [application_id]);
-
-
+        
         return initValues;
+    }
+
+    async getAssessInfo(
+        application_id: number,
+        funding_request_id: number,
+        assessment: AssessmentDTO,
+    ): Promise<AssessmentDTO> {
+
+        let values: Partial<AssessmentDTO> = { ...assessment };
+        
+        values.previous_disbursement = await this.getScalarValue<number>("fn_get_disbursed_amount_fct", [
+            funding_request_id,
+            values.id || 0 // assessment_id because is a preview
+        ]);
+
+        values.net_amount = await this.getScalarValue<number>("fn_get_net_sta", [
+            values.assessed_amount || 0,
+            values.previous_disbursement || 0,
+        ]);
+        
+        values.years_funded = await this.getScalarValue<number>("fn_get_total_funded_years", [this.application.student_id || 0, application_id]);
+
+        values.previous_weeks = await this.getScalarValue<number>("fn_get_previous_weeks_sfa", [
+            "'Normal'",
+            this.application.student_id || 0,
+            this.application.id || 0
+
+        ]);
+        values.assessed_weeks = await this.getScalarValue<number>("fn_get_allowed_weeks", [
+            `'${moment(values.classes_start_date).format("YYYY-MM-DD")}'`,
+            `'${moment(values.classes_end_date).format("YYYY-MM-DD")}'`
+        ]);
+        values.previous_upgrade_weeks = await this.getScalarValue<number>("fn_get_previous_weeks_sfa", [
+            "'Upgrade'",
+            this.application.student_id || 0,
+            this.application.id || 0
+        ]);
+        
+        return values;
+    }
+
+    async refreshData(
+        assessment: AssessmentDTO,
+        disbursementList: DisbursementDTO[],
+        application_id: number,
+    ): Promise<AssessmentDTO> {
+
+        let refreshData: AssessmentDTO = { ...assessment };
+
+        this.application = await this.applicationRepo.getApplicationById(application_id);
+
+        refreshData.second_residence_rate = this.application.is_two_residence
+            ? await this.getScalarValue<number>("fn_get_second_residence_sta", [application_id])
+            : 0;
+            refreshData.travel_allowance = await this.getScalarValue<number>("fn_get_travel_allowance", [
+            refreshData.home_city_id || 0,
+            refreshData.destination_city_id || 0
+        ]);
+        refreshData.weekly_amount = await this.getScalarValue<number>("fn_get_weekly_amount_sta", [application_id]);
+
+        refreshData.assessed_amount = await this.getScalarValue<number>("fn_get_other_inst_total_sta", [
+            refreshData.weekly_amount || 0,
+            refreshData.second_residence_rate,
+            0, // initValues.entitlement_days
+            refreshData.travel_allowance
+        ]);
+        
+        if (disbursementList.length) {
+            let disbursedAmounts = disbursementList.map((d) => {
+                return d.disbursed_amount ?? 0;
+            });
+            refreshData.previous_disbursement = disbursedAmounts.reduce((a, b) => a + b, 0);
+        }
+
+        refreshData.net_amount = await this.getScalarValue<number>("fn_get_net_sta", [
+            refreshData.assessed_amount || 0,
+            refreshData.previous_disbursement || 0,
+        ]);
+        refreshData.years_funded = await this.getScalarValue<number>("fn_get_total_funded_years", [this.application.student_id || 0, application_id]);
+        refreshData.disbursements_required = 1; // always is 1
+        refreshData.previous_weeks = await this.getScalarValue<number>("fn_get_previous_weeks_sfa", [
+            "'Normal'",
+            this.application.student_id || 0,
+            this.application.id || 0
+
+        ]);
+        refreshData.assessed_weeks = await this.getScalarValue<number>("fn_get_allowed_weeks", [
+            `'${moment(refreshData.classes_start_date).format("YYYY-MM-DD")}'`,
+            `'${moment(refreshData.classes_end_date).format("YYYY-MM-DD")}'`
+        ]);
+        refreshData.weeks_allowed = await this.getScalarValue<number>("fn_get_weeks_allowed_sta", [
+            refreshData.previous_weeks || 0,
+            refreshData.assessed_weeks || 0,
+            moment().year() // prev_weeks_curr_yr
+        ]);
+        refreshData.previous_upgrade_weeks = await this.getScalarValue<number>("fn_get_previous_weeks_sfa", [
+            "'Upgrade'",
+            this.application.student_id || 0,
+            this.application.id || 0
+        ]);
+
+        return refreshData;
+    }
+
+    async saveAssessment(
+        assessment_id: number,
+        assessment: AssessmentDTO,
+        disbursementList: DisbursementDTO[],
+    ): Promise<AssessmentDTO> {
+
+        const assessInfo: AssessmentDTO[] = await this.updateAssessment(assessment_id, assessment);
+
+        for (const disburse of disbursementList) {
+            if (disburse?.id) {
+                this.updateDisbursements(disburse);
+            } else {
+                this.insertDisbursements(disburse);
+            }
+        }
+        
+        return assessInfo[0];
+    }
+
+    async insertAssessment(assessment: AssessmentDTO): Promise<any[]> {
+        const filtered = this.getAssessmentTable(assessment);
+        return this.mainDb(this.mainTable).insert(filtered).returning("*");
+    }
+
+    async updateAssessment(id: number, assessment: AssessmentDTO): Promise<any[]> {
+        const filtered = this.getAssessmentTable(assessment);
+        return this.mainDb(this.mainTable)
+            .update(filtered)
+            .where({
+                id: id
+            })
+            .returning("*");
+    }
+
+    async insertDisbursements(disbursement: DisbursementDTO): Promise<any[]> {
+        return this.mainDb("sfa.disbursement").insert(disbursement).returning("*");
+    }
+    async updateDisbursements(disbursement: DisbursementDTO): Promise<any[]> {
+        const id = disbursement.id;
+        delete disbursement.id;
+        return this.mainDb("sfa.disbursement").where({ id }).update(disbursement);
     }
 
 }
--- src/api/routes/admin/sta-assessment-router.ts
+++ src/api/routes/admin/sta-assessment-router.ts
@@ -4,41 +4,123 @@ import knex from "knex";
 import { ReturnValidationErrors } from "../../middleware";
 import { DB_CONFIG } from "../../config";
 import { AssessmentCslftRepository, AssessmentSTA } from "../../repositories";
-import {AssessmentDTO, AssessmentTable} from "../../models";
-import {assessmentRouter} from "./assessment-router";
+import { AssessmentDTO, AssessmentTable } from "../../models";
+import { assessmentRouter } from "./assessment-router";
+import axios from "axios";
 
 const db = knex(DB_CONFIG)
 export const assessmentSTARouter = express.Router();
 const mainTable = "sfa.assessment";
 
 assessmentSTARouter.get("/assess-info/:id",
-[param("id").isInt().notEmpty()], ReturnValidationErrors, 
-async (req: Request, res: Response) => {
+    [param("id").isInt().notEmpty()], ReturnValidationErrors,
+    async (req: Request, res: Response) => {
 
-    const assessmentSTARepo = new AssessmentSTA(db);
-    const { id = undefined } = req.params;
-    let results: Partial<AssessmentDTO> = {};
-    
-    try {
-        const verifyFundingRequest = await db("sfa.funding_request")
-            .where({ id })
-            .first();
+        const assessmentSTARepo = new AssessmentSTA(db);
+        const { id = undefined } = req.params;
+        let results: Partial<AssessmentDTO> = {};
 
-        if (verifyFundingRequest) {
-            results = await assessmentSTARepo.getNewInfo(
-                parseInt(verifyFundingRequest.application_id),
-                parseInt(verifyFundingRequest.id),
-            );
-        }
+        try {
+            const verifyFundingRequest = await db("sfa.funding_request")
+                .where({ id })
+                .first();
 
-        if (Object.keys(results).length > 0) {
-            return res.status(200).json({ success: true, data: results, });
-        } else {
+            if (verifyFundingRequest && verifyFundingRequest?.request_type_id === 1) {
+                const getAssessment = await db(mainTable)
+                    .where("funding_request_id", verifyFundingRequest.id)
+                    .first();
+                console.log("getAssessment", getAssessment);
+
+                if (getAssessment) {
+                    results = await assessmentSTARepo.getAssessInfo(
+                        parseInt(verifyFundingRequest.application_id),
+                        parseInt(verifyFundingRequest.id),
+                        getAssessment
+                    );
+                } else {
+                    results = await assessmentSTARepo.getNewInfo(
+                        parseInt(verifyFundingRequest.application_id),
+                        parseInt(verifyFundingRequest.id),
+                        []
+                    );
+                }
+            }
+
+            if (Object.keys(results).length > 0) {
+                return res.status(200).json({ success: true, data: results, });
+            } else {
+                return res.status(404).send();
+            }
+
+        } catch (error: any) {
+            console.log(error);
             return res.status(404).send();
         }
-
-    } catch (error: any) {
-        console.log(error);
-        return res.status(404).send();
     }
-});
+);
+assessmentSTARouter.post("/",
+    async (req: Request, res: Response) => {
+        try {
+            const { assessment = undefined } = req.body;
+
+            const assessmentRepo = new AssessmentSTA(db);
+
+            const notNullValues = {
+                student_contrib_exempt: "No",
+                spouse_contrib_exempt: "No",
+                student_contribution_review: "No",
+                spouse_contribution_review: "No",
+                parent_contribution_review: "No",
+            };
+
+            let newApp: AssessmentDTO = {
+                ...assessment,
+                ...notNullValues
+            };
+
+            const newRow = await assessmentRepo.insertAssessment(newApp);
+
+            if (newRow && newRow.length === 1) {
+                return res.json({ data: { id: newRow[0].id }, messages: [{ text: "Assessment created", variant: "success" }] });
+            }
+
+            return res.status(404).send();
+        }
+        catch (err) {
+            console.log(err);
+            return res.json({ messages: [{ text: `Saved failed`, variant: "error" }] })
+        }
+    }
+);
+
+// assessmentSTARouter.get("/disbursements/:assessment_id",
+//     [param("assessment_id|").isInt().notEmpty()], ReturnValidationErrors,
+//     async (req: Request, res: Response) => {
+
+//         const assessmentSTARepo = new AssessmentSTA(db);
+//         const { id = undefined } = req.params;
+//         let results: Partial<AssessmentDTO> = {};
+
+//         try {
+//             const verifyFundingRequest = await db("sfa.disbursement")
+//                 .where({ id })
+//                 .first();
+
+//             if (verifyFundingRequest) {
+//                 results = await assessmentSTARepo.getNewInfo(
+//                     parseInt(verifyFundingRequest.application_id),
+//                     parseInt(verifyFundingRequest.id),
+//                 );
+//             }
+
+//             if (Object.keys(results).length > 0) {
+//                 return res.status(200).json({ success: true, data: results, });
+//             } else {
+//                 return res.status(404).send();
+//             }
+
+//         } catch (error: any) {
+//             console.log(error);
+//             return res.status(404).send();
+//         }
+//     });
