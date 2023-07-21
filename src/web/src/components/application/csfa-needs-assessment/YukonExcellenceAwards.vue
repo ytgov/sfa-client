a57cbb034944ea2ec047066a0699c921af0e70cb
Edgar Allan Glez <edgarallanglez@Edgars-MacBook-Pro.local>
2023-07-21 13:29:49 -0700

    trigger recalculation for receipts received

--- src/web/src/components/application/csfa-needs-assessment/YukonExcellenceAwards.vue
+++ src/web/src/components/application/csfa-needs-assessment/YukonExcellenceAwards.vue
@@ -199,7 +199,7 @@
                   disabled
                   @keypress="validate.isNumber($event)"
                   readonly
-                  :value="application.calculated_data.yea_used"
+                  :value="customAssessment.read_only_data.yea_used"
                 ></v-text-field>
               </div>
             </div>
@@ -247,7 +247,7 @@
                   readonly
                   disabled
                   @keypress="validate.isNumber($event)"
-                  :value="(application.calculated_data.yea_earned - application.calculated_data.yea_used)"
+                  :value="(customAssessment.read_only_data.yea_balance)"
                 ></v-text-field>
               </div>
             </div>
