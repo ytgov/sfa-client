f8b2992cdfbdf0590c2303580742ad642e9aa3ed
Marlen Brunner <klondikemarlen@gmail.com>
2023-07-25 11:36:40 -0700

    :sparkles: Add error handling to application letter router.

--- src/api/routes/admin/application-letter-router.ts
+++ src/api/routes/admin/application-letter-router.ts
@@ -15,14 +15,24 @@ applicationLetterRouter.get(
     async (req: Request, res: Response) => {
         const applicationId = parseInt(req.params.applicationId);
 
-        const applicationLetterService = new ApplicationLetterService({ applicationId });
-        const approvalLeter = await applicationLetterService.generateApprovalLetter();
-        const fileName = applicationLetterService.buildApprovalLetterFileName();
+        try {
+            const applicationLetterService = new ApplicationLetterService({ applicationId })
+            const fileName = await applicationLetterService.buildApprovalLetterFileName();
+            const approvalLetter = await applicationLetterService.generateApprovalLetter();
 
-        res.setHeader("Content-disposition", `attachment; filename="${fileName}"`);
-        res.setHeader("Content-type", "application/pdf");
-        res.send(approvalLeter);
+            res.setHeader("Content-disposition", `attachment; filename="${fileName}"`);
+            res.setHeader("Content-type", "application/pdf");
+            res.send(approvalLetter);
+        } catch (error) {
+            if (error instanceof Error) {
+                if (error.message.includes("not found")) {
+                    res.status(404).send(error);
+                } else {
+                    res.status(422).send(error);
+                }
+            } else {
+                res.status(500).send(error);
+            }
+        }
     }
-
-    // res.status(404).send();
 )
