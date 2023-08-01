f18a75afd6d317e872ffe1e849e0e18d29c37820
Marlen Brunner <klondikemarlen@gmail.com>
2023-08-01 15:33:29 -0700

    :construction: Add basic student application drafts wiring.

--- src/api/routes/portal/index.ts
+++ src/api/routes/portal/index.ts
@@ -2,9 +2,11 @@ import express, { Request, Response } from "express";
 import { portalApplicationRouter } from "./application-router";
 import { portalReferenceRouter } from "./reference-router";
 import { portalStudentRouter } from "./student-router";
-import ApplicationsController from "@/controllers/portal/students/student-applications-controller"
+import StudentApplicationsController from "@/controllers/portal/students/student-applications-controller"
+import StudentApplicationDraftsController from "@/controllers/portal/students/student-application-drafts-controller"
 
-const applicationsController = new ApplicationsController();
+const studentApplicationsController = new StudentApplicationsController();
+const studentApplicationDraftsController = new StudentApplicationDraftsController();
 export const portalRouter = express.Router();
 
 portalRouter.use("/student", portalStudentRouter);
@@ -15,7 +17,7 @@ portalRouter.get("/", (req: Request, res: Response) => {
   res.send("portalRouterIndex");
 });
 
-portalRouter.get("/students/:studentId/applications", applicationsController.listStudentApplications)
-portalRouter.get("/students/:studentId/applications/:applicationId", applicationsController.getStudentApplication)
-// portalRouter.get("/students/:studentId/application-drafts", applicationDraftsController.listStudentApplicationDrafts)
-// portalRouter.get("/students/:studentId/draft-applications/:applicationId", applicationDraftsController.getStudentApplicationDraft)
+portalRouter.get("/students/:studentId/applications", studentApplicationsController.listStudentApplications)
+portalRouter.get("/students/:studentId/applications/:applicationId", studentApplicationsController.getStudentApplication)
+portalRouter.get("/students/:studentId/application-drafts", studentApplicationDraftsController.listStudentApplicationDrafts)
+portalRouter.get("/students/:studentId/draft-applications/:applicationDraftId", studentApplicationDraftsController.getStudentApplicationDraft)
