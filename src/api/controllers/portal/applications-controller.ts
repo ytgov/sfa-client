6bc6289d27525c5864cb99281d530ef854b3ed30
Marlen Brunner <klondikemarlen@gmail.com>
2023-08-01 11:33:21 -0700

    :construction: Begin fleshing out application controller and services.

--- src/api/controllers/portal/applications-controller.ts
+++ src/api/controllers/portal/applications-controller.ts
@@ -1,10 +1,20 @@
 import { Request, Response } from "express"
 
+import { ApplicationService } from "../../services/portal";
+
 export default class ApplicationsController {
   listStudentApplications(req: Request, res: Response) {
     const studentId = parseInt(req.params.studentId)
 
-    res.send(`list student applications for Student#${studentId}`)
+    const applicationService = new ApplicationService({ studentId })
+    applicationService
+      .getApplications()
+      .then((applications) => {
+        res.json({ data: applications })
+      })
+      .catch((error: { message: string }) => {
+        res.status(404).json({ error: error.message })
+      })
   }
 
   getStudentApplication(req: Request, res: Response) {
@@ -13,4 +23,6 @@ export default class ApplicationsController {
 
     res.send(`get student application for Application#${applicationId} with Student#${studentId}`)
   }
+
+  #getStudent(studentId: number) {}
 }
