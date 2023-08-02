b87b42b6f8bef15bf7904002fdddcc56faa3fc7e
Marlen Brunner <klondikemarlen@gmail.com>
2023-08-02 11:19:27 -0700

    :heavy_plus_sign: Add camelCasing of db keys to db-client.
    You can also do the reverse via wrap identifier https://knexjs.org/guide/#wrapidentifier
    
    NOTE: can't upgrade to camelcase-keys v8.0 until we are using ESModules.

--- src/api/db/db-client.ts
+++ src/api/db/db-client.ts
@@ -1,7 +1,19 @@
-import knex, { Knex } from "knex";
+import knex from "knex"
+import camelcaseKeys from "camelcase-keys"
 
 import { DB_CONFIG } from "@/config";
 
-const db =  knex(DB_CONFIG);
+const db = knex({
+  ...DB_CONFIG,
+  postProcessResponse: (result, queryContext) => {
+    if (Array.isArray(result)) {
+      // For SELECT queries
+      return result.map((row) => camelcaseKeys(row, { deep: true }))
+    } else {
+      // for INSERT/UPDATE/DELETE queries
+      return camelcaseKeys(result, { deep: true })
+    }
+  },
+})
 
-export default db;
+export default db
