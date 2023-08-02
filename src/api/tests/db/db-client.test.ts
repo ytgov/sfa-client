91100d43e1eb489e61e2c3a9ff5cc5ab9b6e65cf
Marlen Brunner <klondikemarlen@gmail.com>
2023-08-02 12:16:46 -0700

    :mute: Fix warning about handing processes.
    > A worker process has failed to exit gracefully and has been force exited. This is likely caused by tests leaking due to improper teardown. Try running with --detectOpenHandles to find leaks. Active timers can also cause this, ensure that .unref() was called on them.
    
    The database instance should probably be loaded globally or in some kind of helper and close globally as well, rather than just in this file.

--- src/api/tests/db/db-client.test.ts
+++ src/api/tests/db/db-client.test.ts
@@ -1,7 +1,8 @@
 import db from '@/db/db-client';
 
-test('database client connect correctly', () => {
-  return db("sfa.application").where({ id: 250 }).first().then(application => {
-    expect(application).not.toBe(null);
-  })
+afterAll(() => db.destroy())
+
+test('database client connect correctly', async () => {
+  const application = await db("sfa.application").where({ id: 250 }).first();
+  expect(application).not.toBe(null);
 });
