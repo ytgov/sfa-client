e612558171902efef581c112ed27bd7096660894
Marlen Brunner <klondikemarlen@gmail.com>
2023-08-04 14:58:05 -0700

    :recycle: Add support for named approval/rejection letters.
    These should probably be ids or a restricted set of constants or something for safety.
    I think just making them strings would open up the possibility a file system enumeration, but it depends on whether hbs performs sanitiation or not.

--- src/api/services/admin/application-letter-service.ts
+++ src/api/services/admin/application-letter-service.ts
@@ -1,89 +1,84 @@
-
-import { Application } from 'models';
-import { renderViewAsPdf } from '../../utils/express-handlebars-pdf-client';
-import db from '../../db/db-client'
+import { Application } from "models"
+import { renderViewAsPdf } from "../../utils/express-handlebars-pdf-client"
+import db from "../../db/db-client"
 
 export class ApplicationLetterService {
-    #applicationId: number;
-    #applicationData: any;
+  #applicationId: number
+  #fundingType: string
+  #applicationData: any
 
-    constructor({ applicationId } : { applicationId: number; }) {
-        this.#applicationId = applicationId;
+  constructor({ applicationId, fundingType }: { applicationId: number; fundingType: string }) {
+    this.#applicationId = applicationId
+    this.#fundingType = fundingType
+  }
+
+  async generateApprovalLetter(): Promise<Buffer> {
+    await this.#getApplicationData()
+
+    return renderViewAsPdf(`./templates/admin/application-letter/approval/${this.#fundingType}`, {
+      ...this.#applicationData,
+      title: "Application Approval Letter",
+    })
+  }
+
+  async generateRejectionLetter(): Promise<Buffer> {
+    await this.#getApplicationData()
+
+    return renderViewAsPdf(`./templates/admin/application-letter/rejection/${this.#fundingType}`, {
+      ...this.#applicationData,
+      title: "Application Rejection Letter",
+    })
+  }
+
+  ////
+  // See https://xkcd.com/1179/ -> https://en.wikipedia.org/wiki/ISO_8601 for date format
+  async buildApprovalLetterFileName() {
+    await this.#getApplicationData()
+
+    const studentLastName = this.#applicationData.student.person.last_name
+    if (!studentLastName) {
+      Promise.reject(new Error("No student last name"))
     }
 
-    async generateApprovalLetter(): Promise<Buffer> {
-        await this.#getApplicationData()
+    const formattedData = new Date().toISOString().slice(0, 10) // YYYYY-MM-DD
+    return `Approval Letter, ${studentLastName}, ${formattedData}.pdf`
+  }
 
-        return renderViewAsPdf(
-            './templates/admin/application-letter/approval',
-            {
-                ...this.#applicationData,
-                title: 'Application Approval Letter'
-            }
-        )
+  async buildRejectionLetterFileName() {
+    await this.#getApplicationData()
+
+    const studentLastName = this.#applicationData.student.person.last_name
+    if (!studentLastName) {
+      Promise.reject(new Error("No student last name"))
     }
 
-    async generateRejectionLetter(): Promise<Buffer> {
-        await this.#getApplicationData()
+    const formattedData = new Date().toISOString().slice(0, 10) // YYYYY-MM-DD
+    return `Rejection Letter, ${studentLastName}, ${formattedData}.pdf`
+  }
 
-        return renderViewAsPdf(
-            './templates/admin/application-letter/rejection',
-            {
-                ...this.#applicationData,
-                title: 'Application Rejection Letter'
-            }
-        )
+  // Private Methods
+  async #getApplicationData(): Promise<any> {
+    if (this.#applicationData) return this.#applicationData
+
+    const application = await db("sfa.application").where({ id: this.#applicationId }).first()
+    if (!application) {
+      return Promise.reject(new Error("Application not found"))
     }
 
-    ////
-    // See https://xkcd.com/1179/ -> https://en.wikipedia.org/wiki/ISO_8601 for date format
-    async buildApprovalLetterFileName() {
-        await this.#getApplicationData()
-
-        const studentLastName = this.#applicationData.student.person.last_name
-        if (!studentLastName) {
-            Promise.reject(new Error('No student last name'))
-        }
-
-        const formattedData = new Date().toISOString().slice(0, 10); // YYYYY-MM-DD
-        return `Approval Letter, ${studentLastName}, ${formattedData}.pdf`
+    const student = await db("sfa.student").where({ id: application.student_id }).first()
+    if (!student) {
+      return Promise.reject(new Error("Student not found"))
     }
 
-    async buildRejectionLetterFileName() {
-        await this.#getApplicationData()
-
-        const studentLastName = this.#applicationData.student.person.last_name
-        if (!studentLastName) {
-            Promise.reject(new Error('No student last name'))
-        }
-
-        const formattedData = new Date().toISOString().slice(0, 10); // YYYYY-MM-DD
-        return `Rejection Letter, ${studentLastName}, ${formattedData}.pdf`
+    const person = await db("sfa.person").where({ id: student.person_id }).first()
+    if (!person) {
+      return Promise.reject(new Error("Person not found"))
     }
 
-    // Private Methods
-    async #getApplicationData(): Promise<any> {
-        if (this.#applicationData) return this.#applicationData;
+    student.person = person
+    application.student = student
 
-        const application = await db('sfa.application').where({id: this.#applicationId}).first();
-        if (!application) {
-            return Promise.reject(new Error('Application not found'));
-        }
-
-        const student = await db('sfa.student').where({ id: application.student_id }).first();
-        if (!student) {
-            return Promise.reject(new Error('Student not found'));
-        }
-
-        const person = await db('sfa.person').where({ id: student.person_id }).first();
-        if (!person) {
-            return Promise.reject(new Error('Person not found'));
-        }
-
-        student.person = person;
-        application.student = student;
-
-        this.#applicationData = application;
-        return this.#applicationData
-    }
+    this.#applicationData = application
+    return this.#applicationData
+  }
 }
