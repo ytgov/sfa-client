f383e43d1834fbac8f4fe2d9b56d63b11218d076
Marlen Brunner <klondikemarlen@gmail.com>
2023-08-08 08:33:17 -0700

    :recycle: Use singular name for model.

--- src/api/models/funding-source.ts
+++ src/api/models/funding-source.ts
@@ -1,7 +1,7 @@
 // These are fundingRequest.requestType.description values
 // I'm not sure if these should be references only from the database, or computed from table fields?
-enum FundingSources {
+enum FundingSource {
   CANADA_STUDENT_FINANCIAL_ASSISTANCE_FULL_TIME = "Canada Student Financial Assistance (Full-Time)"
 }
 
-export default FundingSources
+export default FundingSource
