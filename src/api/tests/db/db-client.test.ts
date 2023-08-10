bdc04e74442eb438c2b0d26b5590fc6920ea251e
Marlen Brunner <klondikemarlen@gmail.com>
2023-08-10 11:51:00 -0700

    :recycle: Set default schema in test setup.
    Note that there are no migrations in the test setup, so all database tests will fail.

--- src/api/tests/db/db-client.test.ts
+++ src/api/tests/db/db-client.test.ts
@@ -2,7 +2,8 @@ import db from '@/db/db-client';
 
 afterAll(() => db.destroy())
 
+// TODO: set up migrations such
 test('database client connect correctly', async () => {
-  const application = await db("sfa.application").where({ id: 250 }).first();
+  const application = await db("application").where({ id: 250 }).first();
   expect(application).not.toBe(null);
 });
