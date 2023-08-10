b551b0559db3f82cf28579669a9c13aaaa8ce4ae
Marlen Brunner <klondikemarlen@gmail.com>
2023-08-10 16:18:43 -0700

    :ok_hand: Avoid removing/renaming legacy migrations.
    Why? They exist in production and the the Knex system will crash if it can't find all the migrations that it expects.
    
    They can be deleted once the relevant rows are removed from the dbo.knex_migrations table in production.

---
+++ src/api/data/migrations/002_missing_draft_fields.ts
@@ -0,0 +1,6 @@
+// LEGACY: required until deleted from dbo.knex_migrations table production, after wich point they can be removed
+import { Knex } from "knex"
+
+exports.up = async function (knex: Knex, Promise: any) {}
+
+exports.down = async function (knex: Knex, Promise: any) {}
