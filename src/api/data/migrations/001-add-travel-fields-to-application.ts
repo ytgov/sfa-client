09923e4542f710f88f3e798ad88fec6c27a28449
Marlen Brunner <klondikemarlen@gmail.com>
2023-08-10 10:50:33 -0700

    :art: Prettify travel fields migration and genericize schema.
    Note: using default knex for schema migrations.

--- src/api/data/migrations/001-add-travel-fields-to-application.ts
+++ src/api/data/migrations/001-add-travel-fields-to-application.ts
@@ -1,25 +1,29 @@
-import { Knex } from "knex";
+import { Knex } from "knex"
 
-exports.up = async function (knex: Knex, Promise: any) {
-  knex.schema
-    .table('sfa.application_draft', function(table) {
-      table.integer('application_id').nullable();
-    })
-    .table('sfa.application', function(table) {
-      table.integer('has_last_travel').nullable();
-      table.integer('last_travel_year').nullable();
-      table.integer('last_travel_month').nullable();
-    });
-};
+import { DB_CONFIG } from "@/config"
 
-exports.down = async function (knex: Knex, Promise: any) {
-  knex.schema
-    .table('sfa.application_draft', function(table) {
-      table.dropColumn('application_id');
+export function up(knex: Knex) {
+  return knex.schema
+    .withSchema(DB_CONFIG.defaultSchema)
+    .table("application_draft", (table) => {
+      table.integer("application_id").nullable()
     })
-    .table('sfa.application', function(table) {
-      table.dropColumn('has_last_travel');
-      table.dropColumn('last_travel_year');
-      table.dropColumn('last_travel_month');
-    });
-};
+    .table("application", (table) => {
+      table.integer("has_last_travel").nullable()
+      table.integer("last_travel_year").nullable()
+      table.integer("last_travel_month").nullable()
+    })
+}
+
+export function down(knex: Knex) {
+  knex.schema
+    .withSchema(DB_CONFIG.defaultSchema)
+    .table("application_draft", (table) => {
+      table.dropColumn("application_id")
+    })
+    .table("application", (table) => {
+      table.dropColumn("has_last_travel")
+      table.dropColumn("last_travel_year")
+      table.dropColumn("last_travel_month")
+    })
+}
