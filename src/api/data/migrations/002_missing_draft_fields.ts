42ea4cbf9c0acd0fbd7745ff8230ee884592f862
Marlen Brunner <klondikemarlen@gmail.com>
2023-08-09 18:01:27 -0700

    :recycle: Support magic schema injection in migration.
    How? by using the hacked db schema instance instead.
    This is clearly not a wining strategy.

--- src/api/data/migrations/002_missing_draft_fields.ts
+++ src/api/data/migrations/002_missing_draft_fields.ts
@@ -1,52 +1,50 @@
 import { Knex } from "knex";
+import camelcaseKeys from "camelcase-keys"
+
+import db from "@/db/db-client"
+
 import { studentCategoryToCSLClassification } from "@/models";
 
-import { DB_CONFIG } from '@/config'
-
-const DEFAULT_SCHEMA = DB_CONFIG.defaultSchema
-
-exports.up = async function (knex: Knex, Promise: any) {
-  let subs = await knex("application_draft").withSchema(DEFAULT_SCHEMA).where({ status: "Submitted" });
-
+exports.up = async function (_: Knex) {
+  let subs = await db("applicationDraft").where({ status: "Submitted" });
   for (let sub of subs) {
     let app
     try {
-      app = JSON.parse(sub.application_json);
+      app = JSON.parse(sub.applicationJson);
+      app = camelcaseKeys(app, { deep: true })
     } catch (e) {
       console.log("Failed to parse application json: " + e)
       continue
     }
 
-    let attendance_id = app.program_details.attendance == "Full Time" ? 1 : 2;
-    let has_last_travel = app.residency.has_traveled;
-    let last_travel_year = app.residency.last_return_date ? app.residency.last_return_date.split("/")[0] : null;
-    let last_travel_month = app.residency.last_return_date ? app.residency.last_return_date.split("/")[1] : null;
-    let csl_classification = studentCategoryToCSLClassification(app.personal_details.category);
-    let prestudy_csl_classification = studentCategoryToCSLClassification(app.personal_details.category);
+    let attendanceId = app.programDetails.attendance == "Full Time" ? 1 : 2;
+    let hasLastTravel = app.residency.hasTraveled;
+    let lastTravelYear = app.residency.lastReturnDate ? app.residency.lastReturnDate.split("/")[0] : null;
+    let lastTravelMonth = app.residency.lastReturnDate ? app.residency.lastReturnDate.split("/")[1] : null;
+    let cslClassification = studentCategoryToCSLClassification(app.personalDetails.category);
+    let prestudyCslClassification = studentCategoryToCSLClassification(app.personalDetails.category);
 
-    console.log("HAS_TRAV", has_last_travel); //
-    console.log("HAS_TRAV_RET", last_travel_year, last_travel_month);
-    console.log("ATTEND", attendance_id);
-    console.log("CSLCLASS", csl_classification);
-    console.log("PSCSLCLASS", prestudy_csl_classification);
+    console.log("HAS_TRAV", hasLastTravel); //
+    console.log("HAS_TRAV_RET", lastTravelYear, lastTravelMonth);
+    console.log("ATTEND", attendanceId);
+    console.log("CSLCLASS", cslClassification);
+    console.log("PSCSLCLASS", prestudyCslClassification);
 
-    let studentApps = await knex("application")
-      .withSchema(DEFAULT_SCHEMA)
-      .where({ student_id: sub.student_id, academic_year_id: 2023 });
+    let studentApps = await db("application")
+      .where({ studentId: sub.studentId, academicYearId: 2023 });
 
     console.log("STUDENT HAS: ", studentApps.length);
 
     for (let submittedApp of studentApps) {
-      await knex("application")
-        .withSchema(DEFAULT_SCHEMA)
+      await db("application")
         .where({ id: submittedApp.id })
         .update({
-          attendance_id,
-          has_last_travel,
-          last_travel_year,
-          last_travel_month,
-          csl_classification,
-          prestudy_csl_classification,
+          attendanceId,
+          hasLastTravel,
+          lastTravelYear,
+          lastTravelMonth,
+          cslClassification,
+          prestudyCslClassification,
         });
     }
 
