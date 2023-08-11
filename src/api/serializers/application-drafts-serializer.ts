0202cffa39b7cbb4976ffc36bbdf03e29f131105
Marlen Brunner <klondikemarlen@gmail.com>
2023-08-11 11:06:05 -0700

    :sparkles: Return application draft application json as object instead of string.
    Why? Its much easier to work with an manipulate this way.

--- src/api/serializers/application-drafts-serializer.ts
+++ src/api/serializers/application-drafts-serializer.ts
@@ -1,36 +1,66 @@
-import { isArray, pick } from "lodash"
+import { isArray } from "lodash"
 
+import Application from "@/models/application"
 import ApplicationDraft from "@/models/application-draft"
 
 export default class ApplicationsSerializer {
   #applicationDrafts: ApplicationDraft[] = []
-  #applicationDraft: ApplicationDraft | {} = {}
+  #applicationDraft: ApplicationDraft = {} as ApplicationDraft
 
   constructor(draftOrDrafts: ApplicationDraft[] | ApplicationDraft) {
     if (isArray(draftOrDrafts)) {
-      this.#applicationDrafts = draftOrDrafts || []
+      this.#applicationDrafts = draftOrDrafts || ([] as ApplicationDraft[])
     } else {
-      this.#applicationDraft = draftOrDrafts || {}
+      this.#applicationDraft = draftOrDrafts || ({} as ApplicationDraft)
     }
   }
 
   asListView() {
     return this.#applicationDrafts.map((applicationDraft) => {
-      return pick(applicationDraft, [
-        "id",
-        "studentId",
-        "academicYearId",
-        "createDate",
-        "updateDate",
-        "isActive",
-        "submitDate",
-        "status",
-        "description",
-      ])
+      return {
+        id: applicationDraft.id,
+        studentId: applicationDraft.studentId,
+        academicYearId: applicationDraft.academicYearId,
+        createDate: applicationDraft.createDate,
+        updateDate: applicationDraft.updateDate,
+        isActive: applicationDraft.isActive,
+        submitDate: applicationDraft.submitDate,
+        status: applicationDraft.status,
+        description: "TODO", // Does not exist on model
+      }
     })
   }
 
   asDetailedView() {
-    return this.#applicationDraft
+    const application = this.#parseApplicationJson(this.#applicationDraft.applicationJson)
+    return {
+      id: this.#applicationDraft.id,
+      studentId: this.#applicationDraft.studentId,
+      academicYearId: this.#applicationDraft.academicYearId,
+      createDate: this.#applicationDraft.createDate,
+      updateDate: this.#applicationDraft.updateDate,
+      isActive: this.#applicationDraft.isActive,
+      submitDate: this.#applicationDraft.submitDate,
+      status: this.#applicationDraft.status,
+      description: "TODO", // Does not exist on model
+      application: this.#applicationAssocation(application),
+    }
+  }
+
+  #parseApplicationJson(applicationJson: string): Application {
+    try {
+      return JSON.parse(applicationJson)
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new Error(`Could not parse application JSON: ${error.message}`)
+      } else {
+        throw new Error(`Could not parse application JSON`)
+      }
+    }
+  }
+
+  #applicationAssocation(application: Application) {
+    // Makes space for future filtering or mutations
+    return application
   }
 }
