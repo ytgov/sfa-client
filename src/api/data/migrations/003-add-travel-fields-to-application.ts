71f362679851f19e7af41f42ab0db4d059dcea16
Marlen Brunner <klondikemarlen@gmail.com>
2023-08-10 20:08:54 -0700

    :beetle: Make add travel files up migration idempotent.
    I left this migration in, instead of deleting it, as it is a very good example.

--- src/api/data/migrations/003-add-travel-fields-to-application.ts
+++ src/api/data/migrations/003-add-travel-fields-to-application.ts
@@ -2,17 +2,35 @@ import { Knex } from "knex"
 
 import { DB_CONFIG } from "@/config"
 
-export function up(knex: Knex) {
-  return knex.schema
-    .withSchema(DB_CONFIG.defaultSchema)
-    .table("application_draft", (table) => {
+export async function up(knex: Knex) {
+  const schema = knex.schema.withSchema(DB_CONFIG.defaultSchema)
+
+  // Check and add column for 'application_draft' table
+  const hasApplicationIdOnApplicationDraft = await schema.hasColumn(
+    "application_draft",
+    "application_id"
+  )
+  await schema.table("application_draft", (table) => {
+    if (!hasApplicationIdOnApplicationDraft) {
       table.integer("application_id").nullable()
-    })
-    .table("application", (table) => {
+    }
+  })
+
+  // Check and add columns for 'application' table
+  const hasLastTravelOnApplication = await schema.hasColumn("application", "has_last_travel")
+  const hasLastYearOnApplication = await schema.hasColumn("application", "last_travel_year")
+  const hasLastMonthOnApplication = await schema.hasColumn("application", "last_travel_month")
+  await schema.table("application", async (table) => {
+    if (!hasLastTravelOnApplication) {
       table.integer("has_last_travel").nullable()
+    }
+    if (!hasLastYearOnApplication) {
       table.integer("last_travel_year").nullable()
+    }
+    if (!hasLastMonthOnApplication) {
       table.integer("last_travel_month").nullable()
-    })
+    }
+  })
 }
 
 export function down(knex: Knex) {
