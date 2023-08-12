ef1d8a6bcc0524c8530043b28ec04ed68ac502a8
mario-bizont <mariolizarraga07@gmail.com>
2023-08-11 18:57:26 -0600

    Change migration script

--- db/migrations/013_msfaa_rec_dates.sql
+++ db/migrations/013_msfaa_rec_dates.sql
@@ -1,2 +1,2 @@
-ALTER TABLE [sfa].[msfaa] ADD [rec_create_date] DATE NOT NULL DEFAULT GETDATE(); 
-ALTER TABLE [sfa].[msfaa] ADD [rec_last_mod_date] DATE NOT NULL DEFAULT GETDATE(); 
+ALTER TABLE [sfa].[msfaa] ADD [rec_create_date] DATETIME2(0) NOT NULL DEFAULT GETDATE(); 
+ALTER TABLE [sfa].[msfaa] ADD [rec_last_mod_date] DATETIME2(0) NULL; 
