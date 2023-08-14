5a0c68f8482754906e5b5e97e987a242a0abb6fa
Marlen Brunner <klondikemarlen@gmail.com>
2023-08-14 11:11:17 -0700

    :pencil2: Fix wording around dependent eligibility relationships.

--- src/api/models/dependent.ts
+++ src/api/models/dependent.ts
@@ -15,7 +15,7 @@ export default interface Dependent {
   isDisability?: boolean
   relationship?: Relationship
   // NOTE: there isn't a constraint forcing the dependent -> dependent eligibility relationship to be one to one.
-  // While, this seems like the obvious intent of the system, the data is not one-to-one so I can't treat it that way.
+  // While this seems like the obvious intent of the system, the data is not one-to-one so I can't treat it that way.
   // In the future, we will probably want to enforce the one-to-one constraint.
   dependentEligibilities?: DependentEligibility[]
 }
