27da0968e627cdf6d3c8b5149d52efcfedb13177
Marlen Brunner <klondikemarlen@gmail.com>
2023-08-14 08:36:35 -0700

    :tophat: Add dependent eligibility  modeling.

--- src/api/models/dependent.ts
+++ src/api/models/dependent.ts
@@ -1,3 +1,4 @@
+import DependentEligibility from "@/models/dependent-eligibility"
 import Relationship from "@/models/relationship"
 
 // TODO: normalize everything to one spelling, currently in code its sometimes spelled as dependant instead of dependent.
@@ -13,4 +14,8 @@ export default interface Dependent {
   isConversion?: boolean
   isDisability?: boolean
   relationship?: Relationship
+  // NOTE: there isn't a constraint forcing the dependent -> dependent eligibility relationship to be one to one.
+  // While, this seems like the obvious intent of the system, the data is not one-to-one so I can't treat it that way.
+  // In the future, we will probably want to enforce the one-to-one constraint.
+  dependentEligibilities?: DependentEligibility[]
 }
