dad30236c204e6ac433b1e5702b2ecc80c7c1a14
Marlen Brunner <klondikemarlen@gmail.com>
2023-08-14 11:21:44 -0700

    :bug: Fix has dependents check in student application serializer.
    Since value is an array I need to do an isEmpty check not an isNil check.

--- src/api/serializers/applications/student-applications-serializer.ts
+++ src/api/serializers/applications/student-applications-serializer.ts
@@ -335,7 +335,7 @@ export default class StudentApplicationsSerializer {
     })
 
     return {
-      hasDependants: !isNil(serializedDependents),
+      hasDependants: !isEmpty(serializedDependents),
       dependants: serializedDependents,
     }
   }
