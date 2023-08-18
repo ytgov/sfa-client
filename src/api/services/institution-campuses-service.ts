73e60e3b11c967242733f3d27ecee758bd640c26
Marlen Brunner <klondikemarlen@gmail.com>
2023-08-18 15:11:56 -0700

    :beetle: Fix some lookups and add missed model.

--- src/api/services/institution-campuses-service.ts
+++ src/api/services/institution-campuses-service.ts
@@ -25,7 +25,7 @@ export default class InstitutionCampusesService {
   // OPINION: if you want this to be faster, switch to Sequelize.
   // It would be more worthwhile than writing these as massive dynamic queries and recreating an ORM.
   async find(id: number): Promise<InstitutionCampus> {
-    const institutionCampus = await db("institutionCampus")
+    const institutionCampus = await db<InstitutionCampus>("institutionCampus")
       .where({ id })
       .first()
       .then((institutionCampus) => {
@@ -34,7 +34,7 @@ export default class InstitutionCampusesService {
         throw new Error("Institution not found")
       })
 
-    if (this.#includes.includes("institution")) {
+    if (this.#includes.includes("institution") && institutionCampus.institutionId !== undefined) {
       institutionCampus.institution = await db("institution")
         .where({ id: institutionCampus.institutionId })
         .first()
