62ae45338d6861a42aad225c0a3103c8f24a48c5
Marlen Brunner <klondikemarlen@gmail.com>
2023-08-23 09:30:08 -0700

    :japanese_castle: Increase safety of yukon grant student approval serializer.
    How? Make sure any missing required fields with throw an error.

--- src/api/serializers/funding-requests/letters/yukon-grant-student-approval-template-serializer.ts
+++ src/api/serializers/funding-requests/letters/yukon-grant-student-approval-template-serializer.ts
@@ -1,5 +1,9 @@
+import { isNil, last, sortBy } from "lodash"
+
 import Application from "@/models/application"
 import FundingRequest from "@/models/funding-request"
+import Person from "@/models/person"
+import PersonAddress from "@/models/person-address"
 import User from "@/models/user"
 
 export default class YukonGrantStudentApprovalTemplateSerializer {
@@ -24,7 +28,10 @@ export default class YukonGrantStudentApprovalTemplateSerializer {
     fundingRequest: FundingRequest
     signingOfficer: User
   }) {
-    const serializer = new YukonGrantStudentApprovalTemplateSerializer({ fundingRequest, signingOfficer })
+    const serializer = new YukonGrantStudentApprovalTemplateSerializer({
+      fundingRequest,
+      signingOfficer,
+    })
     return serializer.prepare()
   }
 
@@ -38,13 +45,18 @@ export default class YukonGrantStudentApprovalTemplateSerializer {
     const person = application?.student?.person
     if (person === undefined)
       throw new Error("Could not prepare template data as person is missing from student.")
+    if (isNil(person.firstName))
+      throw new Error("Could not prepare template data as firstName is missing from person.")
+    if (isNil(person.lastName))
+      throw new Error("Could not prepare template data as lastName is missing from person.")
 
-    const primaryAddress = application?.primaryAddress
-    if (primaryAddress === undefined)
+    const primaryAddress = this.#preparePrimaryAddress(application, person)
+    if (isNil(primaryAddress.address1))
+      throw new Error("Could not prepare template data as address1 is missing from primaryAddress.")
+    if (isNil(primaryAddress.postalCode))
       throw new Error(
-        "Could not prepare template data as primary address is missing from application."
+        "Could not prepare template data as postalCode is missing from primaryAddress."
       )
-    // TODO: fall back to student.person.personAddress
 
     const city = primaryAddress.city
     if (city === undefined)
@@ -69,24 +81,37 @@ export default class YukonGrantStudentApprovalTemplateSerializer {
       throw new Error(
         "Could not prepare template data as assessments is missing from funding request."
       )
-    if (assessments.length > 1) // TODO: consider returning only the last assessment?
-      throw new Error("Could not prepare template data as multiple assessments are not supported.")
+    if (assessments.length > 1)
+      console.warn(
+        "More than one assessment detected for funding request. This system state has not been investigated."
+      )
 
-    const assessment = assessments[0]
+    const assessment = last(sortBy(assessments, ["assessedDate"]))
     if (assessment === undefined)
       throw new Error(
         "Could not prepare template data as assessment is missing from funding request."
       )
+    if (isNil(assessment.classesStartDate))
+      throw new Error(
+        "Could not prepare template data as classesStartDate is missing from studyArea."
+      )
+    if (isNil(assessment.classesEndDate))
+      throw new Error(
+        "Could not prepare template data as classesEndDate is missing from studyArea."
+      )
 
     const institutionName = this.#prepareInstitutionName(application)
 
-    if (assessment.weeklyAmount === undefined)
+    if (isNil(assessment.weeklyAmount))
       throw new Error("Could not prepare template data as assessement weekly amount is missing.")
     const ratePerWeekInCents = assessment.weeklyAmount * 100
 
-    if (assessment.airfareAmount === undefined)
+    if (isNil(assessment.weeksAllowed))
+      throw new Error("Could not prepare template data as weeksAllowed is missing from assessment.")
+
+    if (isNil(assessment.airfareAmount))
       throw new Error("Could not prepare template data as assessement airfaire amount is missing.")
-    if (assessment.travelAllowance === undefined)
+    if (isNil(assessment.travelAllowance))
       throw new Error("Could not prepare template data as assessement travel allowance is missing.")
     const travelAndAirFairCostInCents =
       (assessment.airfareAmount + assessment.travelAllowance) * 100
@@ -99,7 +124,8 @@ export default class YukonGrantStudentApprovalTemplateSerializer {
         firstName: person.firstName,
         initials: person.initials,
         lastName: person.lastName,
-        address: primaryAddress.address1,
+        address1: primaryAddress.address1,
+        address2: primaryAddress.address2,
         city: city.description,
         province: province.description,
         country: country.description,
@@ -119,6 +145,16 @@ export default class YukonGrantStudentApprovalTemplateSerializer {
     }
   }
 
+  #preparePrimaryAddress(application: Application, person: Person): PersonAddress {
+    const primaryAddress = application?.primaryAddress
+    if (primaryAddress === undefined)
+      throw new Error(
+        "Could not prepare template data as primary address is missing from application."
+      )
+    // TODO: fall back to student.person.personAddress
+    return primaryAddress
+  }
+
   #prepareDisbursements(fundingRequest: FundingRequest) {
     const disbursements = fundingRequest.disbursements
     if (disbursements === undefined)
@@ -133,9 +169,6 @@ export default class YukonGrantStudentApprovalTemplateSerializer {
         throw new Error(
           "Could not prepare template data as disbursement disbursed amount is missing."
         )
-
-      if (disbursement.issueDate === undefined)
-        throw new Error("Could not prepare template data as disbursement issue date is missing.")
 
       return {
         amountInCents: disbursement.disbursedAmount * 100,
