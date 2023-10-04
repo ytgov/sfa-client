a0277a8d624e7b86d63893926c0938b960e91fd2
Michael Johnson <michael@icefoganalytics.com>
2023-10-04 11:20:51 -0700

    Using better Disbursement component on CSLFT

--- src/web/src/components/application/csfa-needs-assessment/CSLFT-detail/CSLFTAward.vue
+++ src/web/src/components/application/csfa-needs-assessment/CSLFT-detail/CSLFTAward.vue
@@ -119,13 +119,7 @@
                 </div>
                 <div class="col-xs-12 col-sm-5 col-lg-5 nopadding d-flex flex-wrap">
                   <div class="col-xs-12 col-lg-12">
-                    <v-btn 
-                        dense
-                        color="blue" 
-                        class="my-0"
-                        block
-                        @click="executeRecalc"
-                      >
+                    <v-btn dense color="blue" class="my-0" block @click="executeRecalc">
                       RE-CALC
                     </v-btn>
                   </div>
@@ -209,13 +203,7 @@
                 <div class="col-xs-12 col-sm-5 col-lg-5 nopadding d-flex flex-wrap">
                   <div class="col-xs-12 col-lg-12 line-jump-height not-displayed-sx"></div>
                   <div class="col-xs-12 col-lg-12">
-                    <v-btn 
-                        dense
-                        color="blue" 
-                        class="my-0"
-                        block
-                        @click="executeDisburse"
-                      >
+                    <v-btn dense color="blue" class="my-0" block @click="executeDisburse">
                       DISBURSE
                     </v-btn>
                   </div>
@@ -308,22 +296,27 @@
           </div>
         </div>
       </v-card-text>
-    </v-card>    
-    <DisbursementList
-      :disbursementList="cslft_disbursement"
-      :disbursementRemove="'removeCslftDisbursement'"
-    >
-    </DisbursementList>
+    </v-card>
+    <div class="mt-4">
+      <v-card class="default mb-5 bg-color-blue">
+        <v-card-text>
+          <csl-disbursements
+            :disbursements="cslft_disbursement"
+            v-on:showError="showError"
+            v-on:showSuccess="showSuccess"
+          ></csl-disbursements>
+        </v-card-text>
+      </v-card>
+    </div>
   </div>
 </template>
 <script>
 import store from "@/store";
 import validator from "@/validator";
-import {mapGetters, mapState} from "vuex";
-import {ref} from "vue";
+import { mapGetters, mapState } from "vuex";
 import { DateHelper } from "@/utilities";
 import DateInput from "../../../DateInput.vue";
-import DisbursementList from "../DisbursementList.vue";
+import CslDisbursements from "../../assessments/components/csl-disbursements.vue";
 
 const dateHelper = new DateHelper();
 
@@ -331,7 +324,7 @@ export default {
   name: "cslft-award",
   components: {
     DateInput,
-    DisbursementList
+    CslDisbursements,
   },
   data() {
     return {
@@ -346,27 +339,27 @@ export default {
   },
   computed: {
     ...mapState({
-      cslft: state => state.cslft.cslft,
-      cslft_disbursement: state => state.cslft.cslft_disbursement
+      cslft: (state) => state.cslft.cslft,
+      cslft_disbursement: (state) => state.cslft.cslft_disbursement,
     }),
     ...mapGetters([
-        "cslft_study_cost_total",
-        "cslft_application_academic_year_id",
-        "cslft_get_resources_total",
-        "cslft_assess_needed",
-        "cslft_assess_needed_sixty_pct",
-        "cslft_max_allowable",
-        "cslft_calculated_award",
-        "cslReasonNonAward",
-        "cslReasonOverAward",
-        "changeReasons",
-        "disbursementTypes",
-        "cslft_get_disbursements",
-        "cslft_get_net_amount"
+      "cslft_study_cost_total",
+      "cslft_application_academic_year_id",
+      "cslft_get_resources_total",
+      "cslft_assess_needed",
+      "cslft_assess_needed_sixty_pct",
+      "cslft_max_allowable",
+      "cslft_calculated_award",
+      "cslReasonNonAward",
+      "cslReasonOverAward",
+      "changeReasons",
+      "disbursementTypes",
+      "cslft_get_disbursements",
+      "cslft_get_net_amount",
     ]),
-    application: function () {
+    application: function() {
       return store.getters.selectedApplication;
-    }
+    },
   },
   methods: {
     executeRecalc() {
@@ -380,33 +373,39 @@ export default {
         return dateHelper.getDateFromUTC(value);
       }
       return null;
-    }
+    },
+    showSuccess(mgs) {
+      this.$emit("showSuccess", mgs);
+    },
+    showError(mgs) {
+      this.$emit("showError", mgs);
+    },
   },
   watch: {
     cslft_calculated_award: {
       immediate: true,
       handler(newValue) {
         store.dispatch("setCslftCalculatedAward", newValue);
-      },      
+      },
     },
     cslft_get_net_amount: {
       immediate: true,
       handler(newVal) {
         store.dispatch("setCslftNetAmount", newVal);
-      }
+      },
     },
     cslft_assess_needed: {
       immediate: true,
       handler(newVal) {
         store.dispatch("setCslftAssessedNeed", newVal);
-      }
-    },  
+      },
+    },
     cslft_disbursement: {
       immediate: true,
       deep: true,
       handler(newVal) {
         if (newVal.length > 0) {
-          let prevDis = 0;          
+          let prevDis = 0;
           newVal.forEach((x) => {
             prevDis += parseInt(x.disbursed_amount);
           });
@@ -419,8 +418,8 @@ export default {
         if (newVal.length === 0) {
           store.dispatch("setDefaultCslftDisbursement");
         }
-      }
-    }
+      },
+    },
   },
   async created() {
     this.validate = validator;
@@ -433,185 +432,183 @@ export default {
     store.dispatch("setCslReasonOverAward");
     store.dispatch("setDisbursementTypes");
     store.dispatch("setChangeReasons");
-  }
+  },
 };
 </script>
 <style>
-  .nopadding {
-    padding: 0 !important;
-  }
-  .nopadding-lr {
-    padding-left: 0 !important;
-    padding-right: 0 !important;
-  }
-  .noppading-bottom,
-  .nopadding-bottom {
-    padding-bottom: 0 !important;
-  }
-  .noppading-top {
-    padding-top: 0 !important;
-  }
-  .nopadding-left {
-    padding-left: 0 !important;
-  }
-  .noppading-right {
-    padding-right: 0 !important;
-  }
-  .padding-top{
-    padding-top: 17px !important;
-  }
-  .equalize-heights {
-    height: 40px;
-  }
-  .border-top {
-    border-top: 3px solid #ccc;
-  }
-  .border-container{
-    border-radius: 4px;
-    border: 1px solid #ccc;
-  }
-  .w-auto{
-    min-width: unset !important;
-    width: 100%;
-  }
-  .bg-color-blue{
-    background-color: #E2F1FD !important;
-  }
-  .low-margin{
-    margin-bottom: 20px !important;
-  }
-  .low-marginx2{
-    margin-bottom: 40px !important;
-  }
-  .top-margin{
-    margin-top: 20px !important;
-  }  
-  .txtarea-width{
-    width: 97.6% !important;
-  }
-  .line-jump-height{
-    height: 64px;
-  }
-  .v-btn:not(.v-btn--round).v-size--default{
-    padding: 0 8px !important;
-  }
-  .cslft-award-assessment .right-block-container > div{
+.nopadding {
+  padding: 0 !important;
+}
+.nopadding-lr {
+  padding-left: 0 !important;
+  padding-right: 0 !important;
+}
+.noppading-bottom,
+.nopadding-bottom {
+  padding-bottom: 0 !important;
+}
+.noppading-top {
+  padding-top: 0 !important;
+}
+.nopadding-left {
+  padding-left: 0 !important;
+}
+.noppading-right {
+  padding-right: 0 !important;
+}
+.padding-top {
+  padding-top: 17px !important;
+}
+.equalize-heights {
+  height: 40px;
+}
+.border-top {
+  border-top: 3px solid #ccc;
+}
+.border-container {
+  border-radius: 4px;
+  border: 1px solid #ccc;
+}
+.w-auto {
+  min-width: unset !important;
+  width: 100%;
+}
+.bg-color-blue {
+  background-color: #e2f1fd !important;
+}
+.low-margin {
+  margin-bottom: 20px !important;
+}
+.low-marginx2 {
+  margin-bottom: 40px !important;
+}
+.top-margin {
+  margin-top: 20px !important;
+}
+.txtarea-width {
+  width: 97.6% !important;
+}
+.line-jump-height {
+  height: 64px;
+}
+.v-btn:not(.v-btn--round).v-size--default {
+  padding: 0 8px !important;
+}
+.cslft-award-assessment .right-block-container > div {
+  border-left: 0px;
+}
+.cslft-award-assessment .right-block-container {
+  border-left: 1px solid #ccc;
+  margin-bottom: 20px;
+}
+.not-displayed-lg {
+  display: none;
+}
+.v-card__title {
+  font-weight: bold !important;
+  font-size: 1.65rem !important;
+}
+.height-fit-content {
+  height: fit-content !important;
+}
+.justify-end {
+  justify-content: flex-end !important;
+}
+.justify-start {
+  justify-content: flex-start !important;
+}
+.justify-center {
+  justify-content: center !important;
+}
+.cslft-award-assessment .right-block-container img {
+  max-height: 80px !important;
+  padding-right: 10px;
+}
+@media (max-width: 1263px) {
+  .cslft-award-assessment .right-block-container {
     border-left: 0px;
   }
-  .cslft-award-assessment .right-block-container{
-    border-left: 1px solid #ccc;
-    margin-bottom: 20px;
+  .v-card__title {
+    font-size: 1.25rem !important;
   }
-  .not-displayed-lg{
+  .not-displayed-lg {
+    display: block;
+    height: 0px;
+    margin: 20px 15px;
+  }
+  .cslft-award-assessment .right-block-container .not-displayed-lg {
+    border-top: 1px solid #ccc;
+  }
+  .not-displayed-sx-md,
+  .d-flex.not-displayed-sx-md {
     display: none;
   }
-  .v-card__title{
-    font-weight: bold !important;
-    font-size: 1.65rem !important;
+  .not-displayed-sx-md,
+  .d-flex.not-displayed-sx-md {
+    height: 0px !important;
+    padding: 0px !important;
   }
-  .height-fit-content{
-    height: fit-content !important;
+}
+@media (max-width: 599px) {
+  .mobile-custom-border {
+    padding: 10px !important;
+    margin: 10px;
+    border: 2px solid #ccc;
+    border-radius: 5px;
   }
-  .justify-end{
-    justify-content: flex-end !important;
+  .mobile-custom-border-2 {
+    padding: 10px !important;
+    margin: 10px;
+    border: 2px solid #ccc;
+    border-radius: 5px;
   }
-  .justify-start{
-    justify-content: flex-start !important;
+  .mobile-custom-border::after {
+    content: "";
+    position: relative;
+    left: 43%;
+    top: 16px;
+    width: 0;
+    height: 0;
+    border-left: 20px solid transparent;
+    border-right: 20px solid transparent;
+    border-top: 15px solid #ccc;
+    clear: both;
   }
-  .justify-center{
-    justify-content: center !important;
+  .nopadding-left {
+    padding-left: 14px !important;
   }
-  .cslft-award-assessment .right-block-container img{
-    max-height: 80px !important;
-    padding-right: 10px;
+  .mobile-noppading-top {
+    padding-top: 0px !important;
   }
-  @media (max-width: 1263px) {
-    .cslft-award-assessment .right-block-container{
-      border-left: 0px;
-    }
-    .v-card__title{
-      font-size: 1.25rem !important;
-    }
-    .not-displayed-lg{
-      display: block;
-      height: 0px;
-      margin: 20px 15px;
-    }
-    .cslft-award-assessment .right-block-container .not-displayed-lg{
-      border-top: 1px solid #ccc;
-    }
-    .not-displayed-sx-md,
-    .d-flex.not-displayed-sx-md{
-      display: none;
-    }
-    .not-displayed-sx-md,
-    .d-flex.not-displayed-sx-md{
-      height: 0px !important;
-      padding: 0px !important;
-    }
+  .mobile-noppading-bottom {
+    padding-bottom: 0px !important;
   }
-  @media (max-width: 599px){
-    .mobile-custom-border{
-      padding: 10px !important;
-      margin: 10px;
-      border: 2px solid #ccc;
-      border-radius: 5px;;
-    }
-    .mobile-custom-border-2{
-      padding: 10px !important;
-      margin: 10px;
-      border: 2px solid #ccc;
-      border-radius: 5px;;
-    }
-    .mobile-custom-border::after{
-      content: '';
-      position: relative;
-      left: 43%;
-      top: 16px;
-      width: 0;
-      height: 0;
-      border-left: 20px solid transparent;
-      border-right: 20px solid transparent;
-      border-top: 15px solid #ccc;
-      clear: both;
-    }
-    .nopadding-left{
-      padding-left: 14px !important;
-    }
-    .mobile-noppading-top{
-      padding-top: 0px !important;
-    }
-    .mobile-noppading-bottom{
-      padding-bottom: 0px !important;
-    }
-    .not-displayed-sx,
-    .d-flex.not-displayed-sx{
-      display: none;
-    }
-    .not-displayed-sx,
-    .d-flex.not-displayed-sx{
-      height: 0px !important;
-      padding: 0px !important;
-    }
-    .mobile-column-flex{
-      flex-direction: column;
-    }
-    .mobile-low-margin{
-      margin-bottom: 20px !important;
-    }
-    .mobile-top-margin{
-      margin-top: 20px !important;
-    }
-    .clss-st-date-re-order{
-      order: 2;
-    }
-    .clss-en-date-re-order{
-      order: 3;
-    }
-    .help-txt-re-order{
-      order: 1;
-    }
+  .not-displayed-sx,
+  .d-flex.not-displayed-sx {
+    display: none;
   }
-
-
+  .not-displayed-sx,
+  .d-flex.not-displayed-sx {
+    height: 0px !important;
+    padding: 0px !important;
+  }
+  .mobile-column-flex {
+    flex-direction: column;
+  }
+  .mobile-low-margin {
+    margin-bottom: 20px !important;
+  }
+  .mobile-top-margin {
+    margin-top: 20px !important;
+  }
+  .clss-st-date-re-order {
+    order: 2;
+  }
+  .clss-en-date-re-order {
+    order: 3;
+  }
+  .help-txt-re-order {
+    order: 1;
+  }
+}
 </style>
