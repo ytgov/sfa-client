4577c8a10f86ad0354f2a4413135178ef639c703
Michael Johnson <michael@icefoganalytics.com>
2023-10-04 11:21:32 -0700

    Reformat and fix institution name

--- src/web/src/components/SelectedAppHeader.vue
+++ src/web/src/components/SelectedAppHeader.vue
@@ -1,15 +1,25 @@
 <template>
   <div class="mb-1 overline" style="">
-    <div class="float-left">  <strong>{{selectedStudentFullName}}</strong> : {{selectedStudentLocator}}</div>
-    <div class="float-right" v-if="selectedApplication && selectedApplication.institution">  <strong>{{selectedApplication.academic_year_id}}</strong> : {{selectedApplication.institution.name}}</div>
+    <div class="float-left">
+      <strong>{{ selectedStudentFullName }}</strong>
+    </div>
+    <div class="d-none float-right" style="width: 500px">
+      <v-autocomplete small-chips multiple :items="options" dense outlined></v-autocomplete>
+    </div>
+    <div class="float-right" v-if="selectedApplication && selectedApplication.institution">
+      <strong>{{ selectedApplication.academic_year_id }}</strong> : {{ selectedApplication.main_institution.name }}
+    </div>
     <hr style="clear:both" />
   </div>
 </template>
 
 <script>
-import {mapState} from "vuex";
+import { mapState } from "vuex";
 
 export default {
   computed: mapState(["selectedStudentFullName", "selectedStudentLocator", "selectedApplication"]),
+  data: () => ({
+    options: ["Director Review"],
+  }),
 };
 </script>
