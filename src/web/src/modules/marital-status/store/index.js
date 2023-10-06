a71e49addf1e41dd7138a55be8a41a7959fb7922
Michael Johnson <michael@icefoganalytics.com>
2023-10-06 14:34:19 -0700

    Reformat

--- src/web/src/modules/marital-status/store/index.js
+++ src/web/src/modules/marital-status/store/index.js
@@ -2,30 +2,30 @@ import axios from "axios";
 import { MARITAL_STATUS } from "@/urls";
 
 const state = {
-    maritalStatusList: [],
+  maritalStatusList: [],
 };
 const getters = {
-    maritalStatusList(state) { 
-        return state.maritalStatusList;
-    },
+  maritalStatusList(state) {
+    return state.maritalStatusList;
+  },
 };
 const mutations = {
-    async SET_MARITAL_STATUS_LIST(state, value) {
-        state.maritalStatusList = value;
-    },
+  async SET_MARITAL_STATUS_LIST(state, value) {
+    state.maritalStatusList = value;
+  },
 };
 const actions = {
-    async setMaritalStatusList(state, value = true) {
-        const res = await axios.get(MARITAL_STATUS+"?filter="+value);
-        if (res?.data?.success) {
-            state.commit("SET_MARITAL_STATUS_LIST", [...res.data.data]);
-        }
-    },
+  async setMaritalStatusList(state, value = true) {
+    const res = await axios.get(MARITAL_STATUS + "?filter=" + value);
+    if (res?.data?.success) {
+      state.commit("SET_MARITAL_STATUS_LIST", [...res.data.data]);
+    }
+  },
 };
 
 export default {
-    state,
-    getters,
-    mutations,
-    actions
+  state,
+  getters,
+  mutations,
+  actions,
 };
