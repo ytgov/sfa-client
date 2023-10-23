a4d5a0a5ee8fd90d224c62d4390bf3d1b227e9e9
Michael Johnson <michael@icefoganalytics.com>
2023-10-23 12:31:04 -0700

    Cleanup need

--- src/web/src/components/application/assessments/store/csl-part-time.js
+++ src/web/src/components/application/assessments/store/csl-part-time.js
@@ -113,28 +113,14 @@ const getters = {
   },
 
   assessedNeed(state) {
-    if (state.assessment) {
-      let mult = state.assessment.study_weeks >= 24 ? 2 : state.assessment.study_weeks > 0 ? 1 : 0;
+    let tuit = state.assessment?.tuition_estimate;
+    let books = state.assessment?.books_supplies_cost;
+    let ptrans = state.assessment?.p_trans_month * state.assessment?.study_months;
+    let daycare =
+      Math.min(state.assessment?.day_care_allowable, state.assessment?.day_care_actual) * state.assessment?.study_months;
+    let totalCosts = tuit + books + ptrans + daycare + dptrans;
 
-      //let reloc = state.assessment.relocation_total || 0;
-      //let disc = Math.min(state.assessment.discretionary_cost, state.assessment.discretionary_cost_actual) || 0;
-      let tuit = state.assessment.tuition_estimate || 0;
-      let books = state.assessment.books_supplies_cost || 0;
-      //let rtrans = state.assessment.r_trans_16wk * mult;
-      //let shelter = state.assessment.shelter_month * state.assessment.study_months;
-      let ptrans = state.assessment.p_trans_month * state.assessment.study_months;
-      let daycare =
-        Math.min(state.assessment.day_care_allowable, state.assessment.day_care_actual) * state.assessment.study_months;
-      //let dshelter = state.assessment.depend_food_allowable * state.assessment.study_months;
-      //let dptrans = state.assessment.depend_tran_allowable * state.assessment.study_months;
-      let totalCosts = tuit + books + ptrans + daycare + dptrans;
-
-      let contribution =
-        (state.assessment.spouse_expected_contribution || 0) + state.assessment.student_expected_contribution;
-
-      return totalCosts > contribution ? totalCosts - contribution : 0;
-    }
-    return 0;
+    return totalCosts > contribution ? totalCosts - contribution : 0;
   },
 
   pastThreshold(state, getters) {
