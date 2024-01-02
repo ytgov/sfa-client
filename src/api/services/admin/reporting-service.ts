f098e2694bdc4cf531e95a0c4d975ba984c63f1d
Michael Johnson <michael@icefoganalytics.com>
2024-01-02 16:54:05 -0700

    STA Report Fix to include correct RT_ID

--- src/api/services/admin/reporting-service.ts
+++ src/api/services/admin/reporting-service.ts
@@ -29,7 +29,7 @@ export default class ReportingService {
       INNER JOIN sfa.student ON application.student_id = student.id
       INNER JOIN sfa.person ON student.person_id = person.id
       INNER JOIN sfa.institution_campus ON application.institution_campus_id = institution_campus.id
-      INNER JOIN sfa .institution ON institution_campus.institution_id = institution.id
+      INNER JOIN sfa.institution ON institution_campus.institution_id = institution.id
       INNER JOIN sfa.marital_status ON application.marital_status_id = marital_status.id
     WHERE application.academic_year_id IN (` +
         years.join(",") +
@@ -57,16 +57,17 @@ export default class ReportingService {
       disbursement.disbursed_amount as net,
       change_reason.description as comment
       FROM sfa.funding_request
-      INNER JOIN sfa.application ON funding_request.application_id = application.id
-      INNER JOIN sfa.student ON application.student_id = student.id
-      INNER JOIN sfa.person ON student.person_id = person.id
-      INNER JOIN sfa.assessment ON assessment.funding_request_id = funding_request.id
-      INNER JOIN sfa.disbursement ON assessment.id = disbursement.assessment_id
-      LEFT OUTER JOIN sfa.change_reason ON disbursement.change_reason_id = change_reason.id
+        INNER JOIN sfa.application ON funding_request.application_id = application.id
+        INNER JOIN sfa.student ON application.student_id = student.id
+        INNER JOIN sfa.person ON student.person_id = person.id
+        INNER JOIN sfa.assessment ON assessment.funding_request_id = funding_request.id
+        INNER JOIN sfa.disbursement ON assessment.id = disbursement.assessment_id
+        LEFT OUTER JOIN sfa.change_reason ON disbursement.change_reason_id = change_reason.id
       WHERE application.academic_year_id IN (2023)
         AND application.institution_campus_id IN (5326, 3488, 5648)
         AND disbursement.issue_date <= GETDATE()
         AND disbursement.due_date IS NULL
+        AND funding_request.request_type_id = 1
       ORDER BY person.last_name, person.first_name`
     );
 
@@ -266,6 +267,7 @@ export default class ReportingService {
           AND application.institution_campus_id IN (5326, 3488, 5648)
           AND disbursement.issue_date <= GETDATE()
           AND disbursement.due_date IS NULL
+          AND funding_request.request_type_id = 1
         ORDER BY person.last_name, person.first_name`
       );
 
@@ -303,6 +305,7 @@ export default class ReportingService {
           AND application.institution_campus_id IN (5326, 3488, 5648)
           AND disbursement.issue_date <= GETDATE()
           AND disbursement.due_date IS NULL
+          AND funding_request.request_type_id = 1
         ORDER BY person.last_name, person.first_name`
       );
 
