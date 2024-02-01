2569cdcd4b6ef1569ba18839ccadba52c8d56dc5
Michael Johnson <michael@icefoganalytics.com>
2024-02-01 15:31:38 -0700

    Reformat

--- src/api/routes/admin/communication-type-router.ts
+++ src/api/routes/admin/communication-type-router.ts
@@ -7,15 +7,11 @@ import { DB_CONFIG } from "../../config";
 const db = knex(DB_CONFIG);
 export const communicationTypeRouter = express.Router();
 communicationTypeRouter.get("/", async (req: Request, res: Response) => {
-  const { filter = true } = req.query;    
+  const { filter = true } = req.query;
   try {
-    const communicationType = await db("sfa.communication_type")  
-    .where("is_active", 1)    
-    .select(
-      "*",
-    );
+    const communicationType = await db("sfa.communication_type").where("is_active", 1).select("*");
 
-    if (communicationType) {      
+    if (communicationType) {
       return res.status(200).json({ success: true, data: [...communicationType] });
     } else {
       return res.status(404).send();
@@ -27,15 +23,11 @@ communicationTypeRouter.get("/", async (req: Request, res: Response) => {
 });
 
 communicationTypeRouter.get("/users", async (req: Request, res: Response) => {
-  const { filter = true } = req.query;    
+  const { filter = true } = req.query;
   try {
-    const user = await db("sfa.[user]")      
-    .select(
-      "id", 
-      "email",
-    );
+    const user = await db("sfa.[user]").select("id", "email");
 
-    if (user) {      
+    if (user) {
       return res.status(200).json({ success: true, data: [...user] });
     } else {
       return res.status(404).send();
@@ -46,19 +38,16 @@ communicationTypeRouter.get("/users", async (req: Request, res: Response) => {
   }
 });
 
-
 communicationTypeRouter.get("/:student_id", async (req: Request, res: Response) => {
   const { filter = true } = req.query;
-  const { student_id } = req.params;      
+  const { student_id } = req.params;
   try {
-    const communication = await db("sfa.communication")  
-    .where("student_id", student_id)    
-    .orderBy("communication_date", "desc")
-    .select(
-      "*",
-    );
+    const communication = await db("sfa.communication")
+      .where("student_id", student_id)
+      .orderBy("communication_date", "desc")
+      .select("*");
 
-    if (communication) {      
+    if (communication) {
       return res.status(200).json({ success: true, data: [...communication] });
     } else {
       return res.status(404).send();
@@ -69,78 +58,98 @@ communicationTypeRouter.get("/:student_id", async (req: Request, res: Response) 
   }
 });
 
+communicationTypeRouter.post(
+  "/communications-log/:student_id",
+  [param("student_id").isInt().notEmpty()],
+  ReturnValidationErrors,
+  async (req: Request, res: Response) => {
+    try {
+      const {
+        officer_id,
+        student_id,
+        request_type_id,
+        communication_type_id,
+        comments,
+        communication_date,
+        show_alert,
+      } = req.body;
+      const currentUser = await db("sfa.[user]").select("id").whereLike("email", `%${officer_id}%`).first();
 
-communicationTypeRouter.post("/communications-log/:student_id",
-[param("student_id").isInt().notEmpty()], ReturnValidationErrors,
-async (req: Request, res: Response) => {        
-    try {      
-        const { officer_id, student_id, request_type_id, communication_type_id, comments, communication_date, show_alert } = req.body;
-        const currentUser = await db("sfa.[user]")
-            .select("id")
-            .whereLike('email', `%${officer_id}%`)        
-            .first();
-            
-            
-        if(!currentUser) {
-          return res.json({ messages: [{ variant: "error", text: "Your user account is not registered, please get in touch with the administrator to solve the problem." }] });
-        }
-        
-        const { application_id } = req.params;
-        const data = {
-            officer_id: currentUser.id, 
-            student_id: student_id, 
-            request_type_id:request_type_id, 
-            communication_type_id:communication_type_id, 
-            comments: comments, 
-            communication_date: communication_date, 
-            show_alert:show_alert}
-        
-            const resInsert = await db("sfa.communication")
-                .insert({ ...data});
-            
-            return resInsert ?
-                res.json({ messages: [{ variant: "success", text: "Saved" }] })
-                :
-                res.json({ messages: [{ variant: "error", text: "Save failed" }] });        
+      if (!currentUser) {
+        return res.json({
+          messages: [
+            {
+              variant: "error",
+              text: "Your user account is not registered, please get in touch with the administrator to solve the problem.",
+            },
+          ],
+        });
+      }
 
-        return res.status(404).send();
+      const { application_id } = req.params;
+      const data = {
+        officer_id: currentUser.id,
+        student_id: student_id,
+        request_type_id: request_type_id,
+        communication_type_id: communication_type_id,
+        comments: comments,
+        communication_date: communication_date,
+        show_alert: show_alert,
+      };
 
+      const resInsert = await db("sfa.communication").insert({ ...data });
+
+      return resInsert
+        ? res.json({ messages: [{ variant: "success", text: "Saved" }] })
+        : res.json({ messages: [{ variant: "error", text: "Save failed" }] });
+
+      return res.status(404).send();
     } catch (error) {
-        console.error(error);
-        return res.status(400).send(error);
-    }    
-});
-
-communicationTypeRouter.put("/communications-log/:student_id",
-    [param("student_id").isInt().notEmpty()],
-    ReturnValidationErrors,
-    async (req: Request, res: Response) => {                
-        const {student_id} = req.params;
-        const { request_type_id, communication_type_id, comments, communication_date, show_alert, id } = req.body;    
-        let { officer_id } = req.body;           
-        try {
-            const currentUser = await db("sfa.[user]")
-            .select("id")
-            .whereLike('email', `%${officer_id}%`)        
-            .first()    
-
-            if(!currentUser) {
-              return res.json({ messages: [{ variant: "error", text: "Your user account is not registered, please get in touch with the administrator to solve the problem." }] });
-            }
-            officer_id = currentUser.id;
-            const resUpdate = await db("sfa.communication")
-                .where({id})                             
-                .update({ officer_id, student_id, request_type_id, communication_type_id, comments, communication_date, show_alert });                
-            return resUpdate ?
-                res.json({ messages: [{ variant: "success", text: "Saved" }] })
-                :
-                res.json({ messages: [{ variant: "error", text: "Failed" }] });
-
-        } catch (error) {
-            console.log(error);
-            return res.json({ messages: [{ text: "Failed to update Funding Request", variant: "error" }] });
-        }
-        
-
+      console.error(error);
+      return res.status(400).send(error);
     }
+  }
+);
+
+communicationTypeRouter.put(
+  "/communications-log/:student_id",
+  [param("student_id").isInt().notEmpty()],
+  ReturnValidationErrors,
+  async (req: Request, res: Response) => {
+    const { student_id } = req.params;
+    const { request_type_id, communication_type_id, comments, communication_date, show_alert, id } = req.body;
+    let { officer_id } = req.body;
+    try {
+      const currentUser = await db("sfa.[user]").select("id").whereLike("email", `%${officer_id}%`).first();
+
+      if (!currentUser) {
+        return res.json({
+          messages: [
+            {
+              variant: "error",
+              text: "Your user account is not registered, please get in touch with the administrator to solve the problem.",
+            },
+          ],
+        });
+      }
+      officer_id = currentUser.id;
+      const resUpdate = await db("sfa.communication")
+        .where({ id })
+        .update({
+          officer_id,
+          student_id,
+          request_type_id,
+          communication_type_id,
+          comments,
+          communication_date,
+          show_alert,
+        });
+      return resUpdate
+        ? res.json({ messages: [{ variant: "success", text: "Saved" }] })
+        : res.json({ messages: [{ variant: "error", text: "Failed" }] });
+    } catch (error) {
+      console.log(error);
+      return res.json({ messages: [{ text: "Failed to update Funding Request", variant: "error" }] });
+    }
+  }
 );
